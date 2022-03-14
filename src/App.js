ff62afa683d42786c7b0088ed46ad21cc197063f
Ayato-kosaka <kosaka.ayato@gmail.com>
2022-03-14 10:44:02 +0000

    微修正

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<YsiList />} />
-        <Route path="/:lang" element={<YsiList />} />
       </Routes>
     </div>
   );
