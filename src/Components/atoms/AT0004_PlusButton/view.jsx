d715fd6c99be859c4e92df4cc3b275974fef21c9
Takapy1 <takahirog1109mai@gmail.com>
2022-03-22 12:31:22 +0900

    PlusButtonにpropswo 追加

--- src/Components/atoms/AT0004_PlusButton/view.jsx
+++ src/Components/atoms/AT0004_PlusButton/view.jsx
@@ -1,6 +1,7 @@
 import { PlusBtn } from './style.js'
 
-export const AT0004_PlusButton = () => {
+export const AT0004_PlusButton = (props) => {
+  const { onClick } = props
   return (
     <PlusBtn onClick={onClick} >
       +
