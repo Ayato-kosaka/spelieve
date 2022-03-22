42f367b742a5b7f5205b2e13d4a3c8b7ef8b01ed
Takapy1 <takahirog1109mai@gmail.com>
2022-03-22 12:46:59 +0900

    [add] FocusOutのprops

--- src/Components/atoms/AT0001_TimeArea/view.jsx
+++ src/Components/atoms/AT0001_TimeArea/view.jsx
@@ -1,10 +1,10 @@
 import { TimeInput, TimeOutput } from './style.js'
 
 export const AT0001_TimeArea = (props) => {
-  const { isInput } = props;
+  const { isInput, onFocusout } = props;
   return (
     <>
-      { isInput ? <TimeInput type="time"></TimeInput> : <TimeOutput></TimeOutput> }
+      { isInput ? <TimeInput type="time" onFocusout={onFocusout}></TimeInput> : <TimeOutput></TimeOutput> }
     </>
   );
 };
--- src/Components/atoms/AT0002_TitleArea/view.jsx
+++ src/Components/atoms/AT0002_TitleArea/view.jsx
@@ -1,7 +1,8 @@
 import { TitileInput } from './style.js'
 
-export const AT0002_TitleArea = () => {
+export const AT0002_TitleArea = (props) => {
+  const { onFocusout } = props
   return (
-    <TitileInput type="text" ></TitileInput>
+    <TitileInput type="text" onFocusout={onFocusout} ></TitileInput>
   )
 }
