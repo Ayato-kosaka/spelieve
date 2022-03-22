02748a0e5a3ae4cc63b0a74eb407faf2ada43fa4
Takapy1 <takahirog1109mai@gmail.com>
2022-03-22 13:56:46 +0900

    [fix] inputタグのFocusOutはonBlurで指定可能

--- src/Components/atoms/AT0002_TitleArea/view.jsx
+++ src/Components/atoms/AT0002_TitleArea/view.jsx
@@ -3,6 +3,6 @@ import { TitileInput } from './style.js'
 export const AT0002_TitleArea = (props) => {
   const { onFocusout } = props
   return (
-    <TitileInput type="text" onFocusout={onFocusout} ></TitileInput>
+    <TitileInput type="text" onBlur={onFocusout} ></TitileInput>
   )
 }
