529a968a9560130cb4d7ab87b314ed534fe5f645
Takapy1 <takahirog1109mai@gmail.com>
2022-06-29 12:20:56 +0900

    displayをuseEffectから外に戻した

--- src/SV0001Itinerary/Components/atoms/AT0001TimeArea/TimeArea.tsx
+++ src/SV0001Itinerary/Components/atoms/AT0001TimeArea/TimeArea.tsx
@@ -1,77 +1,85 @@
 import { StyledInput } from './style'
-import React, { FC, useEffect, useState, FocusEvent, ChangeEvent, KeyboardEvent } from 'react';
+import React, { FC, useEffect, useState, FocusEvent, ChangeEvent } from 'react';
 import * as HK0001Utils from 'SV0000Common/Hooks/HK0001Utils'
 
-// type AT0001TimeAreaProps = {
-//     className: string;
-//     span: Date | null;
-//     hourUnit: string;
-//     minUnit: string;
-//     inputProps: { [key: string]: any }; //役割???
-// }
+type AT0001TimeAreaProps = {
+    className: string;
+    span: Date | undefined;
+    inputProps: { 
+        name: string;
+        onBlur: (event: FocusEvent<HTMLInputElement> ) => void;
+        autoFocus?: boolean;
+     };
+    displayValue: string;
+}
 
 export const AT0001TimeArea: FC<AT0001TimeAreaProps> = ({
+    className,
+    span,
     inputProps,
     displayValue,
-    ...props
 }) => {
-    displayValue ||= HK0001Utils.formatDateToTime(props.value);
-    
-    const [time, setTime] = useState([0,0]); //[hour, min]
+    displayValue ||= HK0001Utils.formatDateToTime(span);
+
+    const [time, setTime] = useState<[number, number]>([0, 0]); //[hour, min]
+    const date: Date = new Date(1970, 0, 1, ...time);
     useEffect(() => {
-        if(props.value){ setTime([props.value.getHours(), props.value.getMinutes()]); }
-    }, [props.value]);
-    const date = new Date(1970, 0, 1, ...time);
-    
-    const [isActive, setIsActive] = useState(false);
-    const [target, setTarget] = useState();
-    const handleFocus = (event) => {
+        if (span) {
+            setTime([span.getHours(), span.getMinutes()])
+        };
+    }, [span]);
+
+    const [isActive, setIsActive] = useState<boolean>(false);
+    const [target, setTarget] = useState<HTMLInputElement>(); // ?
+    const handleFocus = (event: FocusEvent<HTMLInputElement>) => {
         setIsActive(true);
         setTarget(event.target);
     };
-    // Focus in 後のレンダリングで、表示文字が変更されるため、handleFocus 内でなく、useEffect 内で target.select() する
-    useEffect(() => {
-        if (isActive) {
-            target.select();
-        }
-    }, [isActive]);
-    
-    const handleBlur = (event) => {
+    const handleBlur = (event: FocusEvent<HTMLInputElement>) => {
         setIsActive(false);
         inputProps.onBlur(event);
     }
+
+    // Focus in 後のレンダリングで、表示文字が変更されるため、handleFocus 内でなく、useEffect 内で target.select() する
+    useEffect(() => {
+        if (isActive) {
+            target?.select();
+        }
+    }, [isActive]);
+
     
-    const handleChanged = event => {
-        const value = event.target.value;
-        const timeNum = (() => {
-            if (value.length==0) {
+
+    const handleChanged = (event: ChangeEvent<HTMLInputElement>) => {
+        const value: string = event.target.value;
+        const timeNum: number = (() => {
+            if (value.length === 0) {
                 return 0;
-            }else if (value === display().substr(0,display().length-1)){ // BackSpace が入力された場合
-                return Math.floor(parseInt(time.join(''))/10)
+            } else if (value === display().substr(0, display().length - 1)) { // BackSpace が入力された場合
+                return Math.floor(parseInt(time.join('')) / 10)
             } else {
                 const downedKey = parseInt(value[value.length - 1]);
-                if(isNaN(downedKey)){  // 数値以外が入力された場合
+                if (isNaN(downedKey)) {  // 数値以外が入力された場合
                     return NaN;
-                } else if (value.length==1){ // 入力済みの値を削除し数値が入力された場合
+                } else if (value.length == 1) { // 入力済みの値を削除し数値が入力された場合
                     return downedKey;
                 } else { // 数値が追加入力された場合
-                    return (parseInt(time.join(''))*10 + downedKey) % 10000;
+                    return (parseInt(time.join('')) * 10 + downedKey) % 10000;
                 }
             }
         })();
-        if(isNaN(timeNum)){
+        if (isNaN(timeNum)) {
             event.target.select();
             return;
         }
-        setTime(function(hour, min){
+        setTime(function (hour: number, min: number): [number, number] {
             return [
                 (hour > 23) ? hour % 10 : hour,
                 (min > 23 && min % 10 > 6) ? min % 10 : min
             ]
         }(Math.floor(timeNum / 100) % 100, timeNum % 100));
     };
-    
-    const display = () => { return isActive ? HK0001Utils.formatDateToTime(date) : displayValue };
+
+    const display = (): string => { return isActive ? HK0001Utils.formatDateToTime(date) : displayValue };
     return (
         <StyledInput
             size='small'
@@ -84,7 +92,7 @@ export const AT0001TimeArea: FC<AT0001TimeAreaProps> = ({
                 'onBlur': handleBlur,
                 'time': date,
             }}
-            className={props.className}
+            className={className}
         />
     );
 };
