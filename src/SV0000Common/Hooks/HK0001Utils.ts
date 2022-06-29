2dd12baa0e5c485477e47687748085e69d27bd1a
Takapy1 <takahirog1109mai@gmail.com>
2022-06-29 11:08:14 +0900

    js to ts code

--- src/SV0000Common/Hooks/HK0001Utils.ts
+++ src/SV0000Common/Hooks/HK0001Utils.ts
@@ -7,12 +7,12 @@
 // *****************************************************************************
 
 // Date
-export const initialDate = () => new Date(1970, 0, 1, 0, 0, 0);
-export const milliSecondsADay = 24*60*60*1000;
-export const formatDateToTime = (date, hourUnit=':', minUnit='') => {
-    let [hour, min] = [date.getHours(), date.getMinutes()]
+export const initialDate = (): Date => new Date(1970, 0, 1, 0, 0, 0);
+export const milliSecondsADay: number = 24*60*60*1000;
+export const formatDateToTime = (date: Date, hourUnit: string = ':', minUnit: string =''): string => {
+    let [hour, min]: Array<string> = [date.getHours(), date.getMinutes()]
         .map((x) => (String(x).padStart((!minUnit) ? 2 : 1, '0')));
-    if(date.getHours()==0 && minUnit!==''){
+    if(date.getHours() === 0 && minUnit !== ''){
         return (min + minUnit);
     } else {
         return (hour + hourUnit + (parseInt(min)!=0 || !minUnit ? min : ''));
