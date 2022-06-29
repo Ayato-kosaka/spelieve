d19de5d07df8e1d91183a1a842b0dfed72f357e0
Takapy1 <takahirog1109mai@gmail.com>
2022-06-29 12:19:36 +0900

    fix props name at AT0001TimeArea

--- src/SV0001Itinerary/Components/molecules/MC0005DateTime/DateTime.jsx
+++ src/SV0001Itinerary/Components/molecules/MC0005DateTime/DateTime.jsx
@@ -53,10 +53,10 @@ export const MC0005DateTime = ({
                 }}
             />
             <StyledAT0001TimeArea
-                value={ initialDate }
+                span={ initialDate }
                 inputProps={{
-                    'onBlur': handleTimeBlur,
-                    'autoFocus': skipDateFocus
+                    onBlur: handleTimeBlur,
+                    autoFocus: skipDateFocus
                 }}
             />
         </>
