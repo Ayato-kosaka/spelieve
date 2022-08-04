10bbc4cb7739c823370e6ac7ba0691dab2599fc0
Takapy1 <takahirog1109mai@gmail.com>
2022-08-04 16:54:11 +0900

    plan 型定義

--- src/SV0001Itinerary/Utils/types/DB0004PlanType.ts
+++ src/SV0001Itinerary/Utils/types/DB0004PlanType.ts
@@ -1,7 +1,18 @@
+import * as HK0001Utils from 'SV0000Common/Hooks/HK0001Utils'
+
 export type DB0004PlanType = {
     id: string;
-    itineraryID: string;
-    placeId: string;
-    // span: DateTime;
-    imagePath: string;
+    itineraryId: string;
+    title: string;
+    span: Date;
 };
+
+export const initPlan = (id: string, itineraryId: string): DB0004PlanType => {
+    const plan: DB0004PlanType = {
+        id: id,
+        itineraryId: itineraryId,
+        title: '',
+        span: HK0001Utils.initialDate(),
+    };
+    return plan;
+}
