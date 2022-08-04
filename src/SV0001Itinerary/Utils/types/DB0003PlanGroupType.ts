4bb4936595eb4a11eab8cdc68d09d14d374e9803
Takapy1 <takahirog1109mai@gmail.com>
2022-08-04 16:52:41 +0900

    PlanGroupの型定義

--- src/SV0001Itinerary/Utils/types/DB0003PlanGroupType.ts
+++ src/SV0001Itinerary/Utils/types/DB0003PlanGroupType.ts
@@ -1,2 +1,20 @@
+import * as HK0001Utils from 'SV0000Common/Hooks/HK0001Utils'
+
 export type DB0003PlanGroupType = {
+    id: string;
+    itineraryId: string;
+    plans: Array<string>;
+    representativePlanId: string;
+    representativeStartTime: Date;
 };
+
+export const initPlanGroup = (id: string, itineraryId: string): DB0003PlanGroupType => {
+    const planGroup: DB0003PlanGroupType = {
+        id: id,
+        itineraryId: itineraryId,
+        plans: [],
+        representativePlanId: '',
+        representativeStartTime: HK0001Utils.initialDate(),
+    };
+    return planGroup;
+}
