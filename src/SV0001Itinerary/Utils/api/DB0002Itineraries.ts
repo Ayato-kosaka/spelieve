40d2ce0b137064cece38d079bf2a77257a1537f0
Takapy1 <takahirog1109mai@gmail.com>
2022-08-04 13:43:53 +0900

    [fix] typescript化、DB0002ItineraryType定義

---
+++ src/SV0001Itinerary/Utils/api/DB0002Itineraries.ts
@@ -0,0 +1,45 @@
+import db from '../fireB/firestore'
+import { doc, collection, getDoc, setDoc, addDoc, deleteDoc, getDocs } from 'firebase/firestore';
+import type { DB0002ItineraryType } from 'SV0001Itinerary/Utils/types/DB0002ItineraryType';
+import { initItinerary } from 'SV0001Itinerary/Utils/types/DB0002ItineraryType';
+
+export const collectionName = 'Itineraries';
+const collectionRef = collection(db, collectionName);
+
+export const read = async (id: string): Promise<DB0002ItineraryType | null> => {
+    try {
+        let docSnap = await getDoc(doc(collectionRef, id));
+        const itinerary: DB0002ItineraryType = {
+            id: id,
+            title: docSnap.data()?.title,
+        };
+        return itinerary;
+    } catch {
+        return null;
+    }
+}
+
+export const readAll = async (): Promise<DB0002ItineraryType[]> => {
+    const querySnapshot = await getDocs(collectionRef);
+    return (querySnapshot.docs.map((doc) => {
+        const itinerary: DB0002ItineraryType = {
+            id: doc.data().id,
+            title: doc.data().title,
+        };
+        return itinerary;
+    }));
+}
+
+export const create = async (): Promise<DB0002ItineraryType> => {
+    let docRef = await addDoc(collectionRef, {});
+    const itinerary: DB0002ItineraryType = initItinerary(docRef.id);
+    return itinerary;
+}
+
+export const update = async (itinerary: DB0002ItineraryType) => {
+    await setDoc(doc(collectionRef, itinerary.id), itinerary, { merge: true });
+}
+
+export const deleteData = async (itinerary: DB0002ItineraryType) => {
+    await deleteDoc(doc(collectionRef, itinerary.id));
+}
