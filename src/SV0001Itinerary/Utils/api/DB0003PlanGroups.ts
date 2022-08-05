9e0ee440698832df7e5011d12ae8b77e66019468
Takapy1 <takahirog1109mai@gmail.com>
2022-08-05 11:54:05 +0900

    change Id to ID

--- src/SV0001Itinerary/Utils/api/DB0003PlanGroups.ts
+++ src/SV0001Itinerary/Utils/api/DB0003PlanGroups.ts
@@ -6,34 +6,34 @@ import { DB0003PlanGroupType, initPlanGroup } from '../types/DB0003PlanGroupType
 
 const collectionName = 'PlanGroups';
 const parentCollection = collection(db, DB0002Itineraries.collectionName)
-const collectionRef = (itineraryId: string) => collection(parentCollection, itineraryId, collectionName)
+const collectionRef = (itineraryID: string) => collection(parentCollection, itineraryID, collectionName)
 
 
-export const readAll = async (itineraryId: string): Promise<DB0003PlanGroupType[]> => {
-    const querySnapshot = await getDocs(query(collectionRef(itineraryId), orderBy('representiveStartTime')));
+export const readAll = async (itineraryID: string): Promise<DB0003PlanGroupType[]> => {
+    const querySnapshot = await getDocs(query(collectionRef(itineraryID), orderBy('representiveStartTime')));
     return (querySnapshot.docs.map((doc) => {
         const planGroup: DB0003PlanGroupType = {
             id: doc.data().id,
-            itineraryId: doc.data().itineraryId,
+            itineraryID: doc.data().itineraryID,
             plans: doc.data().plans.split(','),
-            representativePlanId: doc.data().representativePlanId,
+            representativePlanID: doc.data().representativePlanID,
             representativeStartTime: doc.data().representativeStartTime.toDate(),
         };
         return planGroup;
     }));
 }
 
-export const create = async (itineraryId: string): Promise<DB0003PlanGroupType> => {
-    let docRef = await addDoc(collectionRef(itineraryId), {});
-    const planGroup: DB0003PlanGroupType = initPlanGroup(docRef.id, itineraryId);
+export const create = async (itineraryID: string): Promise<DB0003PlanGroupType> => {
+    let docRef = await addDoc(collectionRef(itineraryID), {});
+    const planGroup: DB0003PlanGroupType = initPlanGroup(docRef.id, itineraryID);
     return planGroup;
 }
 
 export const update = async (planGroup: DB0003PlanGroupType) => {
-    const setterConverter = (pg: DB0003PlanGroupType) => ({...pg, 'plans': pg.plans.join()}); 
-    await setDoc(doc(collectionRef(planGroup.itineraryId), planGroup.id), setterConverter(planGroup), { merge: true });
+    const setterConverter = (pg: DB0003PlanGroupType) => ({ ...pg, 'plans': pg.plans.join() });
+    await setDoc(doc(collectionRef(planGroup.itineraryID), planGroup.id), setterConverter(planGroup), { merge: true });
 }
 
-export const deleteData = async (planGroup: DB0003PlanGroupType) =>{
-    await deleteDoc(doc(collectionRef(planGroup.itineraryId), planGroup.id));
+export const deleteData = async (planGroup: DB0003PlanGroupType) => {
+    await deleteDoc(doc(collectionRef(planGroup.itineraryID), planGroup.id));
 }
