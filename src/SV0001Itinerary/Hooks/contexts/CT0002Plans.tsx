fb7f1e96f90bbc9b9013770e1ebb297dad0b37c2
Takapy1 <takahirog1109mai@gmail.com>
2022-08-05 12:27:55 +0900

    CT0002Plans.tsxのTypescript化 PlanStateType,CT0002PlansProviderProps,valueTypeの定義

--- src/SV0001Itinerary/Hooks/contexts/CT0002Plans.tsx
+++ src/SV0001Itinerary/Hooks/contexts/CT0002Plans.tsx
@@ -1,19 +1,33 @@
-import React, { ReactNode, useState, createContext, useEffect } from 'react';
+import React, { useState, createContext, useEffect, Dispatch, SetStateAction, ReactNode } from 'react';
 import * as DB0004Plans from 'SV0001Itinerary/Utils/api/DB0004Plans';
 import AT0005Loader from 'SV0001Itinerary/Components/atoms/AT0005Loader';
 import type { DB0004PlanType } from 'SV0001Itinerary/Utils/types/DB0004PlanType';
-
-// createContextに型設定しないといけないっぽい?abCg
-const CT0002Plans = createContext({ plans: {} });
-export default CT0002Plans;
 
 type PlanStateType = {
     [key: string]: DB0004PlanType;
 };
 
-export const CT0002PlansProvider = ({ 
-    itineraryID, children }: { itineraryID: string, children: ReactNode 
-}) => {
+type CT0002PlansProviderProps = {
+    itineraryID: string;
+    children: ReactNode;
+};
+
+type valueType = {
+    plans: PlanStateType,
+    setPlans: Dispatch<SetStateAction<PlanStateType>>;
+    createPlan: () => Promise<DB0004PlanType>;
+    deletePlan: (id: string) => Promise<void>;
+    updatePlan: (plan: DB0004PlanType) => Promise<void>,
+};
+
+// createContextに型設定しないといけないっぽい?abCg
+const CT0002Plans = createContext<valueType>({ plans: {} } as valueType);
+export default CT0002Plans;
+
+
+export const CT0002PlansProvider = (
+    { itineraryID, children }: CT0002PlansProviderProps
+) => {
     const [plans, setPlans] = useState<PlanStateType>({});
     const [isLoading, setIsLoading] = useState<boolean>(true);
 
@@ -47,7 +61,7 @@ export const CT0002PlansProvider = ({
     if (isLoading) {
         return <AT0005Loader />
     }
-    const value = {
+    const value: valueType = {
         plans,
         setPlans,
         createPlan,
