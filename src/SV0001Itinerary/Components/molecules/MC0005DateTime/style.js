ef3d56af59f7a8d62f3c1737c7d4c8ab0afd3368
Ayato-kosaka <kosaka.ayato@gmail.com>
2022-08-12 06:28:23 +0000

    bug fix

--- src/SV0001Itinerary/Components/molecules/MC0005DateTime/style.js
+++ src/SV0001Itinerary/Components/molecules/MC0005DateTime/style.js
@@ -5,7 +5,7 @@ import AT0001TimeArea from 'SV0001Itinerary/Components/atoms/AT0001TimeArea';
 import TextField from '@material-ui/core/TextField';
 
 export const StyledTextField = styled(TextField)`
-    width: 60px;
+    width: 70px;
 `
 
 export const StyledAT0001TimeArea = styled(AT0001TimeArea)`
