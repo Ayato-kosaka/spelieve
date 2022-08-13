778a58ea61705b65b6ac6c7b2239f3062cad7eb8
Takapy1 <takahirog1109mai@gmail.com>
2022-08-13 14:40:51 +0900

    [5ed9f11] の変更対応 HACK: フロント用の型設定、 フロント→DB、DB→フロントへの相互変換メソッドを定義 DBのcreate, update時はDB型に変更してから実行

--- src/SV0001Itinerary/Hooks/contexts/CT0001PlanGroups.tsx
+++ src/SV0001Itinerary/Hooks/contexts/CT0001PlanGroups.tsx
@@ -8,31 +8,41 @@ import type { DB0004PlansType } from 'SV0001Itinerary/Utils/types/DB0004PlansTyp
 
 type CT0001PlanGroupsProviderProps = {
     itineraryID: DB0002ItinerariesType['id'];
-    children: ReactNode;
+    children:    ReactNode;
 };
 
-// type PlansStateType = {
-//     [id: DB0004PlansType['id']]: DB0004PlansType;
-// };
+type FrontPlanGroupsType = Omit<DB0003PlanGroupsType, 'plans'> & { plans: Array<string> };
 
-const CT0001PlanGroups = createContext({});
+type CT0001PlanGroupsContextType = {
+    planGroups:                 FrontPlanGroupsType[];
+    createPlanGroup:            (representativeStartTime: Date) => Promise<void>;
+    updatePlanGroup:            (index: number, planGroup: FrontPlanGroupsType) => Promise<void>;
+    changeRepresentativePlanID: (index: number, planIndex: number)  => void;
+    removePlan:                 (index: number, planIndex: number, isremoveFromPlanGroup: boolean) => void;
+    deletePlan:                 (index: number, planIndex: number) => void;
+    insertPlan:                 (index: number, planIndex: number, planID: DB0004PlansType['id']) => Promise<void>;
+};
+
+const CT0001PlanGroups = createContext<CT0001PlanGroupsContextType>({} as CT0001PlanGroupsContextType);
 export default CT0001PlanGroups;
 
 export const CT0001PlanGroupsProvider = (
     { itineraryID, children }: CT0001PlanGroupsProviderProps
 ) => {
-    const [planGroups, setPlanGroups] = useState<DB0003PlanGroupsType[]>([]);
+    const [planGroups, setPlanGroups] = useState<FrontPlanGroupsType[]>([]);
     const [isLoading, setIsLoading] = useState<boolean>(true);
 
     const { plans, ...useCT0002 } = useContext(CT0002Plans);
 
     useEffect(() => {
         const fetchData = async () => {
-            const planGroups = await DB0003PlanGroups.readAll(itineraryID);
+            const planGroups: DB0003PlanGroupsType[] = await DB0003PlanGroups.readAll(itineraryID);
             if (planGroups.length) {
-                setPlanGroups(planGroups);
-            }
-            else {
+                const frontPlanGroups: FrontPlanGroupsType[] = planGroups.map((planGroup) => {
+                    return toFrontType(planGroup);
+                }); 
+                setPlanGroups(frontPlanGroups);
+            } else {
                 await createPlanGroup();
             }
             setIsLoading(false);
@@ -40,31 +50,40 @@ export const CT0001PlanGroupsProvider = (
         fetchData();
     }, [itineraryID]);
 
+    const toFrontType = (planGroup: DB0003PlanGroupsType): FrontPlanGroupsType => {
+        return { ...planGroup, 'plans': planGroup.plans.split(',') };
+    }
+
+    const toDBType = (planGroup: FrontPlanGroupsType): DB0003PlanGroupsType => {
+        return { ...planGroup, 'plans': planGroup.plans.join() };
+    }
+
     const createPlanGroup = async (representativeStartTime: Date = new Date(1970, 0, 1, 0, 0, 0)): Promise<void> => {
         const plan: DB0004PlansType = await useCT0002.createPlan();
         const planGroup: DB0003PlanGroupsType = await DB0003PlanGroups.create(itineraryID);
-        planGroup.representativePlanID = plan.id;
-        planGroup.representativeStartTime = representativeStartTime;
-        planGroup.plans = [plan.id];
-        DB0003PlanGroups.update(planGroup);
-        setPlanGroups([planGroup, ...planGroups].sort((a: DB0003PlanGroupsType, b: DB0003PlanGroupsType) => a.representativeStartTime.getTime() - b.representativeStartTime.getTime()));
+        const frontPlanGroup: FrontPlanGroupsType = toFrontType(planGroup);
+        frontPlanGroup.representativePlanID  = plan.id;
+        frontPlanGroup.representativeStartTime = representativeStartTime;
+        frontPlanGroup.plans = [plan.id];
+        DB0003PlanGroups.update(toDBType(frontPlanGroup));
+        setPlanGroups([frontPlanGroup, ...planGroups].sort((a: FrontPlanGroupsType, b: FrontPlanGroupsType) => a.representativeStartTime.getTime() - b.representativeStartTime.getTime()));
     }
 
-    const updatePlanGroup = async (index: number, planGroup: DB0003PlanGroupsType) => {
-        DB0003PlanGroups.update(planGroup);
+    const updatePlanGroup = async (index: number, planGroup: FrontPlanGroupsType): Promise<void> => {
+        DB0003PlanGroups.update(toDBType(planGroup));
         planGroups[index] = planGroup;
-        setPlanGroups(planGroups.sort((a: DB0003PlanGroupsType, b: DB0003PlanGroupsType) => a.representativeStartTime.getTime() - b.representativeStartTime.getTime()))
+        setPlanGroups(planGroups.sort((a: FrontPlanGroupsType, b: FrontPlanGroupsType) => a.representativeStartTime.getTime() - b.representativeStartTime.getTime()))
     }
 
-    const deletePlanGroup = (index: number, planGroup: DB0003PlanGroupsType) => {
-        DB0003PlanGroups.deleteData(planGroup);
+    const deletePlanGroup = (index: number, planGroup: FrontPlanGroupsType) => {
+        DB0003PlanGroups.deleteData(toDBType(planGroup));
         setPlanGroups(planGroups.filter((x, i) => i != index));
     }
 
     const changeRepresentativePlanID = (index: number, planIndex: number) => {
         const planGroup = planGroups[index];
         planGroup.representativePlanID = planGroup.plans[planIndex];
-        // planGroup.representativeStartTime = plans[planGroup.representativePlanID].startTime;
+        planGroup.representativeStartTime = plans[planGroup.representativePlanID].startTime;
         setPlanGroups([...planGroups.slice(0, index), planGroup, ...planGroups.slice(index + 1, planGroups.length)]);
     }
 
@@ -75,7 +94,7 @@ export const CT0001PlanGroupsProvider = (
             if (isremoveFromPlanGroup && removedPlanID === planGroup.representativePlanID) {
                 changeRepresentativePlanID(index, 0);
             }
-            DB0003PlanGroups.update(planGroup);
+            DB0003PlanGroups.update(toDBType(planGroup));
             setPlanGroups([...planGroups.slice(0, index), planGroup, ...planGroups.slice(index + 1, planGroups.length)]);
         }
         else {
@@ -89,14 +108,15 @@ export const CT0001PlanGroupsProvider = (
         useCT0002.deletePlan(removedPlanID);
     }
 
-    const insertPlan = async (index: number, planIndex: number, planID: DB0004PlansType['id']) => {
+    const insertPlan = async (index: number, planIndex: number, planID: DB0004PlansType['id']): Promise<void> => {
         const planGroup = planGroups[index];
         if (!planID) {
             const plan = await useCT0002.createPlan();
             planID = plan.id;
         }
         planGroup.plans.splice(planIndex, 0, planID);
-        DB0003PlanGroups.update(planGroup);
+        console.log(planGroup.plans);
+        DB0003PlanGroups.update(toDBType(planGroup));
         setPlanGroups([...planGroups.slice(0, index), planGroup, ...planGroups.slice(index + 1, planGroups.length)]);
     }
 
@@ -104,14 +124,14 @@ export const CT0001PlanGroupsProvider = (
     if (isLoading) {
         return <AT0005Loader />
     }
-    const value = {
+    const value: CT0001PlanGroupsContextType = {
         planGroups,
         createPlanGroup,
+        updatePlanGroup,
         changeRepresentativePlanID,
         removePlan,
         deletePlan,
         insertPlan,
-        updatePlanGroup,
     }
     return <CT0001PlanGroups.Provider value={value}>{children}</CT0001PlanGroups.Provider>
 };
