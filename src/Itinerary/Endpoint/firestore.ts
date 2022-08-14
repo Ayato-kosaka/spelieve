8c6a4b55a372af4c834141acb456b42416b8ca8d
Ayato-kosaka <kosaka.ayato@gmail.com>
2022-08-14 09:35:44 +0000

    development のとき、develop Collection を噛ませるようにした

--- src/Itinerary/Endpoint/firestore.ts
+++ src/Itinerary/Endpoint/firestore.ts
@@ -1,6 +1,10 @@
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore, collection, doc, Firestore } from 'firebase/firestore';
 import app from '@/Common/Endpoint/firebase';
 
 // Firestoreのインスタンス作成
-const db = getFirestore(app);
+let db: Firestore = getFirestore(app);
+if(process.env.NODE_ENV==='development'){
+    db = doc(collection(getFirestore(app), 'develop'), 'GOKyrRFVtAffj6kSzKIc') as unknown as Firestore;
+}
+console.log(process.env.NODE_ENV)
 export default db;
