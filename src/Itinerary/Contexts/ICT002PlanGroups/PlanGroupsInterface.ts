eecb9ed9c0f07ea768e7cfc2593716dd543638b8
Ayato-kosaka <kosaka.ayato@gmail.com>
2022-08-14 10:24:39 +0000

    Weaken の切り出し

--- src/Itinerary/Contexts/ICT002PlanGroups/PlanGroupsInterface.ts
+++ src/Itinerary/Contexts/ICT002PlanGroups/PlanGroupsInterface.ts
@@ -1,9 +1,5 @@
 import { IDB002PlanGroupsInterface } from '@/Itinerary/Interfaces/IDB002PlanGroupsInterface'
-
-// TODO Utils に切り出す
-type Weaken<T, K extends keyof T> = {
-  [P in keyof T]: P extends K ? any : T[P]
-}
+import { Weaken } from '@/Common/Hooks/CHK003TypeScript'
 
 export interface ICT002PlanGroupsInterface extends Weaken<IDB002PlanGroupsInterface, 'plans'> {
     plans: Array<string>;
