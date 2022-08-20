b6cbb55a7399481944901207fb59c8d9bf12584c
Takapy1 <takahirog1109mai@gmail.com>
2022-08-20 12:36:33 +0900

    Itinerariesのcontextのファイル作成

---
+++ src/Itinerary/Contexts/ICT001Itineraries/ItinerariesInterface.ts
@@ -0,0 +1,4 @@
+import { IDB001ItinerariesInterface } from '@/Itinerary/Interfaces/IDB001ItinerariesInterface'
+
+export interface ICT002PlanGroupsInterface extends IDB001ItinerariesInterface { 
+};
