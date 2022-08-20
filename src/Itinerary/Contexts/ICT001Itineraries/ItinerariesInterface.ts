2c8ae7482016f0dee04c8c1ca34cfa8f742b5fdf
Takapy1 <takahirog1109mai@gmail.com>
2022-08-20 17:14:33 +0900

    最新の状態に更新

--- src/Itinerary/Contexts/ICT001Itineraries/ItinerariesInterface.ts
+++ src/Itinerary/Contexts/ICT001Itineraries/ItinerariesInterface.ts
@@ -1,4 +1,4 @@
-import { IDB001ItinerariesInterface } from '@/Itinerary/Interfaces/IDB001ItinerariesInterface'
+import { IDB001ItinerariesInterface } from '@/Itinerary/Models/IDB001Itineraries/IDB001ItinerariesInterface'
 
 export interface ICT001ItinerariesInterface extends IDB001ItinerariesInterface {
 }
