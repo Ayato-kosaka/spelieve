9dad3e7bad2739d43abe164c051e805b4eaa9d93
Ayato-kosaka <kosaka.ayato@gmail.com>
2022-10-20 14:51:59 +0000

    Update ICT011ItineraryOne

--- src/Itinerary/Models/IDB01Itineraries/Contexts/ICT011ItineraryOne/index.ts
+++ src/Itinerary/Models/IDB01Itineraries/Contexts/ICT011ItineraryOne/index.ts
@@ -1,4 +1,2 @@
 export { ICT011ItineraryOne, ICT011ItineraryOneProvider } from './ItineraryOne';
-export * from './ItinerariesInterface';
 export { ICT011ItineraryOneConverter } from './ItinerariesConverter';
-export { ICT011ItineraryOneBuild } from './ItinerariesBuild';
