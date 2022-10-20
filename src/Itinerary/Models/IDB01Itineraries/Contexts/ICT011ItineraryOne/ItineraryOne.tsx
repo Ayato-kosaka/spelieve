167a764b159467696fa614929fecb447890e0ca4
Ayato-kosaka <kosaka.ayato@gmail.com>
2022-10-20 13:11:58 +0000

    Update ICT011ItineraryOne

--- src/Itinerary/Models/IDB01Itineraries/Contexts/ICT011ItineraryOne/ItineraryOne.tsx
+++ src/Itinerary/Models/IDB01Itineraries/Contexts/ICT011ItineraryOne/ItineraryOne.tsx
@@ -1,19 +1,20 @@
 import { ActivityIndicator } from 'react-native-paper';
 import { useState, createContext, useEffect, ReactNode } from 'react';
 import db from '@/Itinerary/Endpoint/firestore'
-import { collection, doc, query, QuerySnapshot, onSnapshot, addDoc, DocumentReference, DocumentSnapshot, QueryDocumentSnapshot } from 'firebase/firestore';
+import { collection, doc, query, QuerySnapshot, onSnapshot, addDoc, DocumentReference, DocumentSnapshot, QueryDocumentSnapshot, setDoc } from 'firebase/firestore';
 import { ICT011ItineraryOneConverter } from './ItinerariesConverter';
 import { Itineraries } from 'spelieve-common/lib/Models/Itinerary/IDB01/Itineraries'
+import { ItineraryOneInterface, ItineraryOneProviderPropsInterface, ItineraryOneValInterface } from 'spelieve-common/lib/Interfaces/Itinerary'
 
-export const ICT011ItineraryOne = createContext({} as ICT011ItineraryOneValInterface);
+export const ICT011ItineraryOne = createContext({} as ItineraryOneInterface);
 
 export const ICT011ItineraryOneProvider = ({
     parentDocRef,
     children,
     id,
-}: ICT011ItineraryOneProviderPropsInterface) => {
+}: ItineraryOneProviderPropsInterface) => {
     
-    const [documentSnapshot, setDocumentSnapshot] = useState<ICT011ItineraryOneValInterface['documentSnapshot'] | null>(null);
+    const [documentSnapshot, setDocumentSnapshot] = useState<DocumentSnapshot<Itineraries> | null>(null);
     
     const collectionRef = parentDocRef
         ?   collection(parentDocRef, Itineraries.modelName).withConverter(ICT011ItineraryOneConverter())
@@ -33,16 +34,20 @@ export const ICT011ItineraryOneProvider = ({
         return unsubscribe()
     }, [parentDocRef, id]);
 
-    const create: ICT011ItineraryOneValInterface['create'] = async () => {
-        return await addDoc<ICT011ItineraryOneInterface>(collectionRef, ICT011ItineraryOneBuild());
+    const create = async () => {
+        // return await addDoc<ICT011ItineraryOneInterface>(collectionRef, ICT011ItineraryOneBuild());
+    }
+
+    const update = async (itinerary: Itineraries) => {
+        return await setDoc<ItineraryOneInterface>(collectionRef, itinerary);
     }
 
     if (!documentSnapshot) {
         return <ActivityIndicator animating={true} />
     }
     
-    const value: ICT011ItineraryOneValInterface = {
-        itinerary: documentSnapshot.id,
+    const value: ItineraryOneValInterface = {
+        itinerary: documentSnapshot.data(),
         reference: documentSnapshot.ref,
         create,
     }
