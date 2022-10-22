a2c738c155db467dc81c614f6f9a592f8d1b1669
Takapy1 <takahirog1109mai@gmail.com>
2022-10-22 23:20:43 +0900

    設計変更に伴う修正

--- src/Place/Pages/PPA001Places/Places.tsx
+++ src/Place/Pages/PPA001Places/Places.tsx
@@ -3,29 +3,37 @@ import { doc, collection, getDoc, setDoc, addDoc, deleteDoc, DocumentSnapshot } 
 // import db from '@/Itinerary/Endpoint/firestore';
 import { ActivityIndicator, Text, Title } from 'react-native-paper';
 
-import { PCT011MPlacesListProvider } from '@/Place/Models/PDB01MPlace/Contexts/PCT011MPlacesList';
-import { PMC01101GoogleMapPlacesList } from '@/Place/Models/PDB01MPlace/Contexts/PCT011MPlacesList/ModelComponents/PMC01101GoogleMapPlaceList';
-import { PMC01102PlacesList } from '@/Place/Models/PDB01MPlace/Contexts/PCT011MPlacesList/ModelComponents/PMC01102PlaceList';
+import { PCT011MPlacesListProvider, PCT011MPlacesList } from '@/Place/Models/PDB01MPlace/Contexts/PCT011MPlacesList';
+import { PPA001PlacesPropsInterface } from 'spelieve-common/lib/Interface';
+import { PMC01101GoogleMapPlacesList } from '@/Place/Models/PDB01MPlace/Contexts/PCT011MPlacesList/ModelComponents/PMC01101GoogleMapPlacesList';
 import { PCO001SearchPlace } from '@/Place/Components/PCO001SearchPlace/SearchPlace';
-import { onAutoCompleteClicked, onPlaceSelected } from './PlacesController';
+import { PMC01102PlacesList } from '@/Place/Models/PDB01MPlace/Contexts/PCT011MPlacesList/ModelComponents/PMC01102PlacesList';
+import { PPA001PlacesController } from './PlacesController';
 
 export const PPA001Places = ({
-    geopoint: Geopoint = new Geopoint(0,0),
-    maxDistance: number = 3000000,
-}) => {
-  if(!placesList){
-      return <ActivityIndicator animating={true} />
-  }
+  geopoint,
+  maxDistance,
+  navigation
+}: PPA001PlacesPropsInterface) => {
   
-  return(
+  const { onAutoCompleteClicked, onPlaceSelected } = PPA001PlacesController();
+  // if(!placesList){
+  //     return <ActivityIndicator animating={true} />
+  // }
+
+  return (
     <PCT011MPlacesListProvider
-        parentDocRef={null}
-        geopoint={geopoint}
-        maxDistance={maxDistance1}
+      parentDocRef={undefined}
+      initialCountry={"日本"}
+      initialAdministrativeAreaLevel1={"東京都"}
+      initialAdministrativeAreaLevel2={""}
+      initialLocality={"渋谷区"}
     >
-        <PMC01101GoogleMapPlacesList />
-        <PCO001SearchPlace />
-        <PMC01102PlacesList />
+      <PMC01101GoogleMapPlacesList />
+      <PCO001SearchPlace
+        onAutoCompleteClicked={onAutoCompleteClicked}
+      />
+      <PMC01102PlacesList />
     </PCT011MPlacesListProvider>
   )
 };
