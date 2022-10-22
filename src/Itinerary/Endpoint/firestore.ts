d29d7efe13e4831dd144fb0afc73b22c7cf9e4bb
Ayato-kosaka <kosaka.ayato@gmail.com>
2022-10-22 08:27:27 +0000

    close #267

--- src/Itinerary/Endpoint/firestore.ts
+++ src/Itinerary/Endpoint/firestore.ts
@@ -4,6 +4,7 @@ import app from '@/Common/Endpoint/firebase';
 
 // Firestoreのインスタンス作成
 let db: Firestore = getFirestore(app);
+
 if (process.env.NODE_ENV === 'development') {
 	console.log(process.env.NODE_ENV);
 	db = doc(collection(getFirestore(app), 'develop'), 'GOKyrRFVtAffj6kSzKIc') as unknown as Firestore;
