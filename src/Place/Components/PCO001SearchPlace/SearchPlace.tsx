bb3ef854bd309c6a6de898b2cdafd093715084f2
Takapy1 <takahirog1109mai@gmail.com>
2022-10-22 23:16:04 +0900

    検索機能実装

--- src/Place/Components/PCO001SearchPlace/SearchPlace.tsx
+++ src/Place/Components/PCO001SearchPlace/SearchPlace.tsx
@@ -1,17 +1,21 @@
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { SearchPlacePropsInterface } from 'spelieve-common/lib/Interfaces';
+import { PCT011MPlacesList } from '@/Place/Models/PDB01MPlace/Contexts/PCT011MPlacesList';
 
-export const PCO001SearchPlace = () => {
+export const PCO001SearchPlace = (
+    { onAutoCompleteClicked, hideDistinct }: SearchPlacePropsInterface
+) => {
+
     return (
         <GooglePlacesAutocomplete
-            placeholder='Search'
-            onPress={(data, details = null) => {
-                // 'details' is provided when fetchDetails = true
-                console.log(data, details);
-            }}
+            placeholder='地点を入力'
+            onPress={onAutoCompleteClicked}
             query={{
-                key: 'YOUR API KEY',
-                language: 'ja',
+                key: process.env.GCP_API_KEY,
+                type: ['(cities)', 'establishment'], // TODO: 地名と地点
+                language: 'ja'
             }}
-    />
+            fetchDetails={true}
+        />
     )
 }
