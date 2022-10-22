d586622cf3193af20c03b741a3b82fa2e961faed
Takapy1 <takahirog1109mai@gmail.com>
2022-10-22 23:15:04 +0900

    spelieve commonに移行につき削除

--- src/Place/Models/PDB01MPlace/Contexts/PCT011MPlacesList/ModelComponents/PMC01101GoogleMapPlaceList.tsx
+++
@@ -1,8 +0,0 @@
-
-export const PMC01101GoogleMapPlacesList = () => {
-    return (
-        <>
-            
-        </>
-    )
-}
--- src/Place/Models/PDB01MPlace/index.ts
+++
@@ -1,7 +0,0 @@
-/**
-* The Firestore Collection name
-*/
-export const collectionName: string = 'MPlace';
-
-export * from './MPlaceInterface';
-export { PDB01MPlaceCols } from './MPlaceCols';
