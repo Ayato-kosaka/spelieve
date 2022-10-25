7483c509907061fd64ff1f8c7bf8c8ee1256eb71
Takapy1 <takahirog1109mai@gmail.com>
2022-10-25 09:51:30 +0900

    微修正

--- src/Place/Components/PCO001SearchPlace/SearchPlace.tsx
+++ src/Place/Components/PCO001SearchPlace/SearchPlace.tsx
@@ -1,15 +1,13 @@
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { SearchPlacePropsInterface } from 'spelieve-common/lib/Interfaces';
-import { PCT011MPlacesList } from '@/Place/Models/PDB01MPlace/Contexts/PCT011MPlacesList';
 
-export const PCO001SearchPlace = (
-    { onAutoCompleteClicked, hideDistinct }: SearchPlacePropsInterface
-) => {
+export function PCO001SearchPlace({ onAutoCompleteClicked }: SearchPlacePropsInterface) {
     
     return (
         <GooglePlacesAutocomplete
             placeholder='地点を入力'
             onPress={onAutoCompleteClicked}
+            autoFocus={true}
             query={{
                 key: process.env.GCP_API_KEY,
                 type: ['(cities)', 'establishment'], // TODO: 地名と地点
--- src/Place/Endpoint/firestore.ts
+++ src/Place/Endpoint/firestore.ts
@@ -2,5 +2,5 @@ import { getFirestore, collection, doc, Firestore } from 'firebase/firestore';
 import app from '@/Common/Endpoint/firebase';
 
 // Firestoreのインスタンス作成
-let db: Firestore = getFirestore(app);
+const db: Firestore = getFirestore(app);
 export default db;
