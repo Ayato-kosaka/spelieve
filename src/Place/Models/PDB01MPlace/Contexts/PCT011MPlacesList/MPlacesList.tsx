308b962b8d30aec2044bd090a28221dcc69172a4
Takapy1 <takahirog1109mai@gmail.com>
2022-10-28 09:30:15 +0900

    refactor query作成

--- src/Place/Models/PDB01MPlace/Contexts/PCT011MPlacesList/MPlacesList.tsx
+++ src/Place/Models/PDB01MPlace/Contexts/PCT011MPlacesList/MPlacesList.tsx
@@ -1,110 +1,107 @@
-import { ActivityIndicator } from 'react-native-paper';
-import { useState, createContext, useEffect } from 'react';
-import { collection, doc, query, QuerySnapshot, onSnapshot, addDoc, where, DocumentReference, DocumentData, DocumentSnapshot, QueryDocumentSnapshot, getDoc, getDocs, orderBy, Query } from 'firebase/firestore';
-import { MPlace } from 'spelieve-common/lib/Models/Place/PDB01/MPlace';
 import {
-    MPlacesListInterface,
-    MPlacesListValInterface,
-    MPlacesListProviderPropsInterface,
+	collection,
+	query,
+	where,
+	getDocs,
+	orderBy,
+	Query,
+    QueryConstraint,
+} from 'firebase/firestore';
+import { useState, createContext, useEffect } from 'react';
+
+import {
+	MPlacesListInterface,
+	MPlacesListValInterface,
+	MPlacesListProviderPropsInterface,
 } from 'spelieve-common/lib/Interfaces/Place';
-import { PCT011MPlacesListConverter } from './MPlacesListConverter';
-import db from '@/Place/Endpoint/firestore'
+import { MPlace } from 'spelieve-common/lib/Models/Place/PDB01/MPlace';
+
+import db from '@/Place/Endpoint/firestore';
 
 export const PCT011MPlacesList = createContext({} as MPlacesListValInterface);
 
 export function PCT011MPlacesListProvider({
-    parentDocRef,
-    children,
-    initialCountry,
-    initialAdministrativeAreaLevel1,
-    initialAdministrativeAreaLevel2,
-    initialLocality,
+	parentDocRef,
+	children,
+	initialCountry,
+	initialAdministrativeAreaLevel1,
+	initialAdministrativeAreaLevel2,
+	initialLocality,
 }: MPlacesListProviderPropsInterface) {
+	const initialAddress = {
+		country: initialCountry,
+		administrativeAreaLevel1: initialAdministrativeAreaLevel1,
+		administrativeAreaLevel2: initialAdministrativeAreaLevel2,
+		locality: initialLocality,
+	};
+	const [placesList, setPlacesList] = useState<MPlacesListInterface[]>([]);
+	const [searchedAddress, setSearchedAddress] = useState(initialAddress);
+	// console.log(placesList);
+	console.log(searchedAddress);
 
-    const initialAddress = {
-        country: initialCountry,
-        administrativeAreaLevel1: initialAdministrativeAreaLevel1,
-        administrativeAreaLevel2: initialAdministrativeAreaLevel2,
-        locality: initialLocality,
-    }
-    const [placesList, setPlacesList] = useState<MPlacesListInterface[]>([]);
-    const [searchedAddress, setSearchedAddress] = useState(initialAddress);
-    // console.log(placesList);
-    console.log(searchedAddress);
+	useEffect(() => {
+		const createPlaceQuery = (): Query => {
+			// const collectionRef = parentDocRef
+			//         ? collection(parentDocRef, MPlace.modelName).withConverter(PCT011MPlacesListConverter())
+			//         : collection(db, MPlace.modelName).withConverter(PCT011MPlacesListConverter());
+			const qc: QueryConstraint[] = [];
+            qc.push(where(MPlace.Cols.country, '==', searchedAddress.country));
+			
+			if (searchedAddress.administrativeAreaLevel1 !== '') {
+				// index済
+				qc.push(where(MPlace.Cols.administrativeAreaLevel1, '==', searchedAddress.administrativeAreaLevel1))
+			}
+			
+			if (searchedAddress.administrativeAreaLevel2 !== '') {
+				qc.push(where(MPlace.Cols.administrativeAreaLevel2, '==', searchedAddress.administrativeAreaLevel2))
+			}
+			
+			if (searchedAddress.locality !== '') {
+				// index済
+				qc.push(where(MPlace.Cols.locality, '==', searchedAddress.locality))
+			}
+			
+			qc.push(orderBy(MPlace.Cols.rating, 'desc'))
+			const placeCollectionRef = collection(db, MPlace.modelName); // .withConverter(PCT011MPlacesListConverter());
 
-    useEffect(()=> {
-        const createPlaceQuery = (): Query => {
-            // const collectionRef = parentDocRef
-            //         ? collection(parentDocRef, MPlace.modelName).withConverter(PCT011MPlacesListConverter())
-            //         : collection(db, MPlace.modelName).withConverter(PCT011MPlacesListConverter());
-            const placeCollectionRef = collection(db, MPlace.modelName); // .withConverter(PCT011MPlacesListConverter());
-            let q: Query = query( // index済
-                                placeCollectionRef,
-                                where(MPlace.Cols.country, '==', searchedAddress.country),
-                                orderBy('rating', 'desc')
-                            );
-            if (searchedAddress.administrativeAreaLevel1 !== ''){ // index済
-                q = query(
-                        placeCollectionRef,
-                        where(MPlace.Cols.country, '==', searchedAddress.country),
-                        where(MPlace.Cols.administrativeAreaLevel1, '==', searchedAddress.administrativeAreaLevel1),
-                        orderBy('rating', 'desc')
-                    );
-            }
+			return query(placeCollectionRef, ...qc);
+		};
 
-            if (searchedAddress.administrativeAreaLevel2 !== ''){
-                q = query(
-                        placeCollectionRef,
-                        where(MPlace.Cols.country, '==', searchedAddress.country),
-                        where(MPlace.Cols.administrativeAreaLevel1, '==', searchedAddress.administrativeAreaLevel1),
-                        where(MPlace.Cols.administrativeAreaLevel2, '==', searchedAddress.administrativeAreaLevel2),
-                        orderBy('rating', 'desc')
-                    );
-            }
+		const fetchUpData = async (q: Query) => {
+			const querySnapshot = await getDocs(q);
+			const places: MPlacesListInterface[] = [];
+			querySnapshot.forEach((doc) => {
+				places.push(doc.data() as MPlacesListInterface);
+			});
+			setPlacesList(places);
+		};
 
-            if (searchedAddress.administrativeAreaLevel2 !== '' && searchedAddress.locality !== '') { // index済
-                q = query(
-                        placeCollectionRef,
-                        where(MPlace.Cols.country, '==', searchedAddress.country),
-                        where(MPlace.Cols.administrativeAreaLevel1, '==', searchedAddress.administrativeAreaLevel1),
-                        where(MPlace.Cols.administrativeAreaLevel2, '==', searchedAddress.administrativeAreaLevel2),
-                        where(MPlace.Cols.locality, '==', searchedAddress.locality),
-                        orderBy('rating', 'desc')
-                    );
-            }
+		const q: Query = createPlaceQuery();
+		fetchUpData(q);
+	}, [searchedAddress]);
 
-            if (searchedAddress.administrativeAreaLevel2 === '' && searchedAddress.locality !== '') { // index済
-                q = query(
-                    placeCollectionRef,
-                    where(MPlace.Cols.country, '==', searchedAddress.country),
-                    where(MPlace.Cols.administrativeAreaLevel1, '==', searchedAddress.administrativeAreaLevel1),
-                    where(MPlace.Cols.locality, '==', searchedAddress.locality),
-                    orderBy('rating', 'desc')
-                );
-            }
-            return q;
-        }
+    // const retrieveMore = async () => {
+    //     // setRefreshing(true);
 
-        const fetchUpData = async (q: Query) => {
-            const querySnapshot = await getDocs(q);
-            const places: MPlacesListInterface[] = [];
-            querySnapshot.forEach(doc => {
-                places.push(doc.data() as MPlacesListInterface);
-            });
-            setPlacesList(places);
-        }
+    //     // Cloud Firestore: Query (Additional Query)
+    //     const placeCollectionRef = collection(db, MPlace.modelName)
+    //     let additionalQuery = query(
+    //         placeCollectionRef,
+    //         where('id', '<=', 20),
+    //         orderBy('id'),
+    //         startAfter(this.state.lastVisible),
+    //         limit(10),
 
-        const q: Query = createPlaceQuery();
-        fetchUpData(q);
-    }, []);
-
-    // if (placesList.length == 0) {
-    //     return <ActivityIndicator animating />
     // }
 
-    const value: MPlacesListValInterface = {
-        placesList,
-        setSearchedAddress,
-    }
-    return <PCT011MPlacesList.Provider value={value}>{children}</PCT011MPlacesList.Provider>
+
+	// if (placesList.length == 0) {
+	//     return <ActivityIndicator animating />
+	// }
+
+	const value: MPlacesListValInterface = {
+		placesList,
+		setSearchedAddress,
+	};
+	return <PCT011MPlacesList.Provider value={value}>{children}</PCT011MPlacesList.Provider>;
 }
