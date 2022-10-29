f949a0613c259e5d6627dbdf083d642fde930d06
Takapy1 <takahirog1109mai@gmail.com>
2022-10-29 09:46:49 +0900

    collection ref

--- src/Place/Models/PDB01MPlace/Contexts/PCT011MPlacesList/MPlacesList.tsx
+++ src/Place/Models/PDB01MPlace/Contexts/PCT011MPlacesList/MPlacesList.tsx
@@ -1,5 +1,6 @@
-import { collection, query, where, getDocs, orderBy, Query, QueryConstraint } from 'firebase/firestore';
+import { collection, query, where, getDocs, orderBy, Query, QueryConstraint, startAfter, limit, DocumentSnapshot, QueryDocumentSnapshot, DocumentData } from 'firebase/firestore';
 import { useState, createContext, useEffect } from 'react';
+import { ActivityIndicator } from 'react-native-paper';
 
 import {
 	MPlacesListInterface,
@@ -26,77 +27,91 @@ export function PCT011MPlacesListProvider({
 		administrativeAreaLevel2: initialAdministrativeAreaLevel2,
 		locality: initialLocality,
 	};
+	const placeCollectionRef = parentDocRef
+		? collection(parentDocRef, MPlace.modelName)
+		: collection(db, MPlace.modelName);
 	const [placesList, setPlacesList] = useState<MPlacesListInterface[]>([]);
+	const [isLoading, setIsLoading] = useState<boolean>(true);
 	const [searchedAddress, setSearchedAddress] = useState(initialAddress);
+	const [lastVisible, setLastVisible] = useState<DocumentSnapshot | null>(null);
+	const [isRefreshing, setIsRefreshing] = useState<boolean>(false);
 
-	useEffect(() => {
-		const createPlaceQuery = (): Query => {
-			// const collectionRef = parentDocRef
-			//         ? collection(parentDocRef, MPlace.modelName).withConverter(PCT011MPlacesListConverter())
-			//         : collection(db, MPlace.modelName).withConverter(PCT011MPlacesListConverter());
-			const qc: QueryConstraint[] = [];
-			qc.push(where(MPlace.Cols.country, '==', searchedAddress.country));
+	const createBasicQueryConstraint = (): QueryConstraint[] => {
+		// const collectionRef = parentDocRef
+		//         ? collection(parentDocRef, MPlace.modelName).withConverter(PCT011MPlacesListConverter())
+		//         : collection(db, MPlace.modelName).withConverter(PCT011MPlacesListConverter());
+		const qc: QueryConstraint[] = [];
+		qc.push(where(MPlace.Cols.country, '==', searchedAddress.country));
 
-			if (searchedAddress.administrativeAreaLevel1 !== '') {
-				// index済
-				qc.push(where(MPlace.Cols.administrativeAreaLevel1, '==', searchedAddress.administrativeAreaLevel1));
-			}
+		if (searchedAddress.administrativeAreaLevel1 !== '') {// index済
+			qc.push(where(MPlace.Cols.administrativeAreaLevel1, '==', searchedAddress.administrativeAreaLevel1));
+		}
 
-			if (searchedAddress.administrativeAreaLevel2 !== '') {
-				qc.push(where(MPlace.Cols.administrativeAreaLevel2, '==', searchedAddress.administrativeAreaLevel2));
-			}
+		if (searchedAddress.administrativeAreaLevel2 !== '') {
+			qc.push(where(MPlace.Cols.administrativeAreaLevel2, '==', searchedAddress.administrativeAreaLevel2));
+		}
 
-			if (searchedAddress.locality !== '') {
-				// index済
-				qc.push(where(MPlace.Cols.locality, '==', searchedAddress.locality));
-			}
+		if (searchedAddress.locality !== '') {// index済
+			qc.push(where(MPlace.Cols.locality, '==', searchedAddress.locality));
+		}
 
-			qc.push(orderBy(MPlace.Cols.rating, 'desc'));
-			const placeCollectionRef = collection(db, MPlace.modelName); // .withConverter(PCT011MPlacesListConverter());
+		qc.push(orderBy(MPlace.Cols.rating, 'desc'));
+		return qc;
+	};
 
-			return query(placeCollectionRef, ...qc);
-		};
-
-		const fetchUpData = async (q: Query) => {
-			const places: MPlacesListInterface[] = [];
-			await getDocs(q)
-				.then((querySnapshot) => {
-					querySnapshot.forEach((doc) => {
+	const fetchUpPlaces = async (q: Query) => {
+		const places: MPlacesListInterface[] = [];
+		let lastDoc: DocumentSnapshot;
+		await getDocs(q)
+			.then((querySnapshot) => {
+				if (!querySnapshot.empty) {
+					const docSize = querySnapshot.size;
+					querySnapshot.forEach((doc, index) => {
+						if (index == docSize-1) {
+							lastDoc = doc;
+						}
 						places.push(doc.data() as MPlacesListInterface);
 					});
-				})
-				.catch((e) => {
-					console.log(e);
-				});
-			setPlacesList(places);
-		};
+				}
+			})
+			.catch((e) => {
+				console.log(e);
+			});
+		setLastVisible(lastDoc);
+		setPlacesList([...placesList, ...places]);
+	};
 
-		const q: Query = createPlaceQuery();
+	const retrieveMore = async () => {
+		setIsRefreshing(true);
+		let qc: QueryConstraint[] = createBasicQueryConstraint();
+		qc.push(startAfter(lastVisible));
+		qc.push(limit(10));
+		const q: Query = query(placeCollectionRef, ...qc);
+		try {
+			await fetchUpPlaces(q);
+			setIsRefreshing(false);
+		} catch(e) {
+			console.log(e);
+		}
+	}
+
+	useEffect(() => {
+		const qc: QueryConstraint[] = createBasicQueryConstraint();
+		qc.push(limit(10));
+		const q: Query = query(placeCollectionRef, ...qc);
 		// eslint-disable-next-line @typescript-eslint/no-floating-promises
-		fetchUpData(q);
+		fetchUpPlaces(q);
+		setIsLoading(false);
 	}, [searchedAddress]);
 
-	// const retrieveMore = async () => {
-	//     // setRefreshing(true);
-
-	//     // Cloud Firestore: Query (Additional Query)
-	//     const placeCollectionRef = collection(db, MPlace.modelName)
-	//     let additionalQuery = query(
-	//         placeCollectionRef,
-	//         where('id', '<=', 20),
-	//         orderBy('id'),
-	//         startAfter(this.state.lastVisible),
-	//         limit(10),
-
-	// }
-
-	// if (placesList.length == 0) {
-	//     return <ActivityIndicator animating />
-	// }
+	if (isLoading) {
+	    return <ActivityIndicator animating />
+	}
 
 	const value: MPlacesListValInterface = {
 		placesList,
 		setSearchedAddress,
+		retrieveMore,
 	};
 	return <PCT011MPlacesList.Provider value={value}>{children}</PCT011MPlacesList.Provider>;
 }
