2d2492eac2fc11a46ef00eb02bb012e9f5559145
Ayato-kosaka <kosaka.ayato@gmail.com>
2022-10-31 11:52:02 +0000

    Update Tab.Navigator

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 import registerRootComponent from 'expo/build/launch/registerRootComponent';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
 
-import { IPA000Test } from '@/Itinerary/Pages/IPA000Test';
-import { NavigationContainer } from '@react-navigation/native';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { PPA000DummyPage } from './Place/Page/PPA000DummyPage';
 
-const styles = StyleSheet.create({
-});
+import { IPA000Test } from '@/Itinerary/Pages/IPA000Test';
+import i18n from './Common/Hooks/i18n-js';
+
+const styles = StyleSheet.create({});
 
 const Tab = createMaterialBottomTabNavigator();
 
@@ -17,12 +17,24 @@ export default function App() {
 	return (
 		<NavigationContainer>
 			<PaperProvider>
-					<Tab.Navigator
-					initialRouteName="Itinerary"
-					>
-						<Tab.Screen name="Itinerary" component={IPA000Test} />
-						<Tab.Screen name="Place" component={PPA000DummyPage} />
-					</Tab.Navigator>
+				<Tab.Navigator initialRouteName="Itinerary">
+					<Tab.Screen
+						name="Itinerary"
+						component={IPA000Test}
+						options={{
+							tabBarLabel: i18n.t('Itinerary'),
+							tabBarIcon: 'book',
+						}}
+					/>
+					<Tab.Screen
+						name="Place"
+						component={PPA000DummyPage}
+						options={{
+							tabBarLabel: i18n.t('Place'),
+							tabBarIcon: 'map-search',
+						}}
+					/>
+				</Tab.Navigator>
 			</PaperProvider>
 		</NavigationContainer>
 	);
