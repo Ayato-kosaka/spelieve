dfc423afbb0463fce66872e47b6fb2a6e341cc8f
Takapy1 <takahirog1109mai@gmail.com>
2022-11-05 12:31:45 +0900

    google map web対応

---
+++ src/Place/Models/PDB01MPlace/Contexts/PCT011MPlacesList/ModelComponents/PMC01101GoogleMapPlacesList/GoogleMapPlacesList.tsx
@@ -0,0 +1,63 @@
+import { useContext, useState } from 'react';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import { GoogleMap, LoadScript, Marker as MarkerWeb } from '@react-google-maps/api'
+
+import { PCT011MPlacesList } from '@/Place/Models/PDB01MPlace/Contexts/PCT011MPlacesList';
+import { Platform } from 'react-native';
+import { ENV } from '@/ENV';
+
+export const PMC01101GoogleMapPlacesList = () => {
+	const { placesList } = useContext(PCT011MPlacesList);
+	const [ref, setRef] = useState<MapView | null>(null);
+
+	const center = placesList[0];
+	const centerLat = center ? center.geometry.latitude : 35.6809591;
+	const centerLng = center ? center.geometry.longitude : 139.7673068;
+
+	if (Platform.OS == 'web') {
+		console.log('webが読み込まれた');
+		const containerStyle = {
+			height: "300px",
+			width: "100%",
+		  };
+
+		const centerPoint = {
+			lat: centerLat,
+			lng: centerLng,
+		};
+		return (
+			<LoadScript googleMapsApiKey={ENV.GCP_API_KEY}>
+				<GoogleMap mapContainerStyle={containerStyle} center={centerPoint} zoom={10}>
+					{placesList.map((place) => {
+						const coordinate = { lat: place.geometry.latitude, lng: place.geometry.longitude };
+						return <MarkerWeb position={coordinate} title={place.name} key={place.place_id} />;
+					})}
+				</GoogleMap>
+        	</LoadScript>
+		);
+	} else {
+		return (
+			<MapView
+				style={{ width: '100%', height: 200 }}
+				provider={PROVIDER_GOOGLE}
+				ref={(r) => setRef(r)}
+				initialRegion={{
+					latitude: centerLat,
+					longitude: centerLng,
+					latitudeDelta: 0.2522, // 地図表示範囲内の北端と南端の緯度の差 大→範囲広
+					longitudeDelta: 0.0521, // 地図表示範囲内の東端と西端の経度の差
+				}}
+				region={{
+					latitude: centerLat,
+					longitude: centerLng,
+					latitudeDelta: 0.2522, // 地図表示範囲内の北端と南端の緯度の差 大→範囲広
+					longitudeDelta: 0.0521, // 地図表示範囲内の東端と西端の経度の差
+				}}>
+				{placesList.map((place) => {
+					const coordinate = { latitude: place.geometry.latitude, longitude: place.geometry.longitude };
+					return <Marker coordinate={coordinate} title={place.name} key={place.place_id} />;
+				})}
+			</MapView>
+		);
+	}
+};
