bd3045cf9df6c273bf0904970428b570936e195b
Ayato-kosaka <kosaka.ayato@gmail.com>
2022-11-06 04:52:58 +0000

    Update IPA002ItineraryCover

--- src/Itinerary/Pages/IPA002ItineraryCover/ItineraryCover.tsx
+++ src/Itinerary/Pages/IPA002ItineraryCover/ItineraryCover.tsx
@@ -1,66 +1,29 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { setDoc } from 'firebase/firestore';
-import { useContext, useEffect, useState } from 'react';
-import { ActivityIndicator, FlatList, Image, TextInputChangeEventData, ScrollView } from 'react-native';
+import { ActivityIndicator, FlatList, Image, ScrollView } from 'react-native';
 import { Chip, TextInput, Searchbar } from 'react-native-paper';
 
-import { ItineraryOneInterface } from 'spelieve-common/lib/Interfaces';
+import { IPA002ItineraryCoverController } from './ItineraryCoverController';
 
 import { BottomTabParamList } from '@/App';
 import i18n from '@/Common/Hooks/i18n-js';
-import { ICT011ItineraryOne } from '@/Itinerary/Models/IDB01Itineraries/Contexts/ICT011ItineraryOne';
 
 export function IPA002ItineraryCover({
 	route,
 	navigation,
 }: NativeStackScreenProps<BottomTabParamList, 'IPA002ItineraryCover'>) {
 	const { itineraryID } = route.params;
+	const { pageItinerary, updateItinerary, handleOnChange, deleteTag, shouldNavigate, isLoading } =
+		IPA002ItineraryCoverController({ itineraryID });
 
-	// TODO: あとで消す ?itineraryID=uMFhF6OQph2UUuKEsKNa
-
-	// ここから Controller
-	const [pageItinerary, setPageItinerary] = useState<ItineraryOneInterface | undefined>(undefined);
-	const { setItineraryID, itineraryDocSnap } = useContext(ICT011ItineraryOne);
-	useEffect(() => {
-		if (itineraryID) {
-			setItineraryID(itineraryID);
-		}
-	}, [itineraryID, setItineraryID]);
-
-	useEffect(() => {
-		if (itineraryDocSnap?.exists()) {
-			setPageItinerary(itineraryDocSnap.data());
-		}
-	}, [itineraryDocSnap]);
-
-	const updateItinerary = () => {
-		// eslint-disable-next-line @typescript-eslint/no-floating-promises
-		setDoc<ItineraryOneInterface>(itineraryDocSnap!.ref, { ...pageItinerary! });
-	};
-
-	const handleOnChange =
-		(column: keyof ItineraryOneInterface) =>
-		({ nativeEvent }: { nativeEvent: TextInputChangeEventData }) => {
-			setPageItinerary({ ...pageItinerary!, [column]: nativeEvent.text });
-		};
-
-	const deleteTag = (index: number): void => {
-		const newTags: string[] = pageItinerary!.tags.splice(index, 1);
-		// eslint-disable-next-line @typescript-eslint/no-floating-promises
-		setDoc<ItineraryOneInterface>(itineraryDocSnap!.ref, { ...pageItinerary!, tags: newTags });
-	};
-
-	const isLoading = !itineraryDocSnap;
-
-	if (!itineraryID || (itineraryDocSnap && !itineraryDocSnap.exists())) {
+	if (shouldNavigate) {
 		navigation.navigate('Itinerary', { screen: 'IPA001ItineraryEdit', params: { itineraryID } });
 	}
-
-	// ここまでController
 
 	if (isLoading || !pageItinerary) {
 		return <ActivityIndicator animating />;
 	}
+	
+	/* TODO: https://github.com/Ayato-kosaka/spelieve/issues/300 Itinerary の style を修正する */
 
 	return (
 		<ScrollView>
@@ -94,16 +57,17 @@ export function IPA002ItineraryCover({
 					</Chip>
 				)}
 				ListFooterComponent={
-					<Searchbar placeholder="Search" value="TODO: https://github.com/Ayato-kosaka/spelieve/issues/298" />
+					/* TODO: https://github.com/Ayato-kosaka/spelieve/issues/298 Tagを取得するSearchBarを実装する */
+					<Searchbar placeholder="Search" value="" />
 				}
 			/>
+			{/* TODO: https://github.com/Ayato-kosaka/spelieve/issues/299 DatePickerを実装する */}
 			<TextInput
 				label={i18n.t('滞在開始日')}
 				value={`${pageItinerary.startDate.getMonth() + 1}/${pageItinerary.startDate.getDate()}`}
 				onChange={handleOnChange('startDate')}
 				onBlur={updateItinerary}
 			/>
-			{/* TODO: https://github.com/Ayato-kosaka/spelieve/issues/299 */}
 			<TextInput
 				label={i18n.t('キャプション')}
 				value={pageItinerary.caption}
--- src/Itinerary/Pages/ItineraryPageNavigator.tsx
+++ src/Itinerary/Pages/ItineraryPageNavigator.tsx
@@ -8,6 +8,8 @@ import { ICT031PlansMapProvider } from '../Models/IDB03Plans/Contexts/ICT031Plan
 
 import { IPA002ItineraryCover } from './IPA002ItineraryCover';
 
+import i18n from '@/Common/Hooks/i18n-js';
+
 export type ItineraryStackParamList = {
 	IPA001ItineraryEdit: ItineraryEditPropsInterface;
 	IPA002ItineraryCover: ItineraryCoverPropsInterface;
@@ -21,7 +23,12 @@ export function ItineraryPageNavigator() {
 			<ICT031PlansMapProvider>
 				<ICT021PlanGroupsListProvider>
 					<Stack.Navigator initialRouteName="IPA002ItineraryCover">
-						<Stack.Screen name="IPA002ItineraryCover" component={IPA002ItineraryCover} initialParams={{}} />
+						<Stack.Screen
+							name="IPA002ItineraryCover"
+							component={IPA002ItineraryCover}
+							initialParams={{}}
+							options={{ title: i18n.t('しおり設定') }}
+						/>
 					</Stack.Navigator>
 				</ICT021PlanGroupsListProvider>
 			</ICT031PlansMapProvider>
