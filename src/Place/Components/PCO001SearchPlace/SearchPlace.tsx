c32237013c8c7879862d46d55358580f9e261f0f
Takapy1 <takahirog1109mai@gmail.com>
2022-11-10 13:37:30 +0900

    hideCities追加

--- src/Place/Components/PCO001SearchPlace/SearchPlace.tsx
+++ src/Place/Components/PCO001SearchPlace/SearchPlace.tsx
@@ -4,15 +4,16 @@ import { SearchPlacePropsInterface } from 'spelieve-common/lib/Interfaces';
 
 import i18n from '@/Common/Hooks/i18n-js';
 import { ENV } from '@/ENV';
+import { useEffect } from 'react';
 
-export const PCO001SearchPlace = ({ onAutoCompleteClicked }: SearchPlacePropsInterface) => (
+export const PCO001SearchPlace = ({ onAutoCompleteClicked, hideCities }: SearchPlacePropsInterface) => (
 	// TODO: https://github.com/Ayato-kosaka/spelieve/issues/309 Web用プロキシサーバ立てる
 	<GooglePlacesAutocomplete
 		placeholder={i18n.t('searchPlaceText')}
 		onPress={onAutoCompleteClicked}
 		query={{
 			key: ENV.GCP_API_KEY,
-			type: ['(cities)', 'establishment'],
+			type: hideCities ? 'establishment' :['(cities)', 'establishment'],
 			language: 'ja', // TODO: https://github.com/Ayato-kosaka/spelieve/issues/281 端末言語情報グローバルに持つ
 		}}
 		requestUrl={{
