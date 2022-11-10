53dac8c3bccbd6c719dcaff4768b7d479ac6b3a3
Takapy1 <takahirog1109mai@gmail.com>
2022-11-10 14:02:06 +0900

    SearchedAddress undefined許容

--- src/Place/Pages/PPA001Places/PlacesController.ts
+++ src/Place/Pages/PPA001Places/PlacesController.ts
@@ -2,7 +2,7 @@ import { useNavigation } from '@react-navigation/native';
 import { useContext } from 'react';
 import { GooglePlaceData, GooglePlaceDetail, AddressComponent } from 'react-native-google-places-autocomplete';
 
-import { PlacesControllerInterface } from 'spelieve-common/lib/Interfaces';
+import { MPlacesListAddressInterface, PlacesControllerInterface } from 'spelieve-common/lib/Interfaces';
 
 import { PCT011MPlacesList } from '@/Place/Models/PDB01MPlace/Contexts/PCT011MPlacesList';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
@@ -35,21 +35,16 @@ export const PPA001PlacesController = (): PlacesControllerInterface => {
 			// 地名
 			const addressParts: AddressComponent[] = details?.address_components || [];
 
-			const searchedAddress = {
-				country: '',
-				administrativeAreaLevel1: '',
-				administrativeAreaLevel2: '',
-				locality: '',
-			};
+			const searchedAddress: MPlacesListAddressInterface = {};
 
 			searchedAddress.country =
-				addressParts.find((addressPart) => addressPart.types.includes('country'))?.long_name || '';
+				addressParts.find((addressPart) => addressPart.types.includes('country'))?.long_name;
 			searchedAddress.administrativeAreaLevel1 =
-				addressParts.find((addressPart) => addressPart.types.includes('administrative_area_level_1'))?.long_name || '';
+				addressParts.find((addressPart) => addressPart.types.includes('administrative_area_level_1'))?.long_name;
 			searchedAddress.administrativeAreaLevel2 =
-				addressParts.find((addressPart) => addressPart.types.includes('administrative_area_level_2'))?.long_name || '';
+				addressParts.find((addressPart) => addressPart.types.includes('administrative_area_level_2'))?.long_name;
 			searchedAddress.locality =
-				addressParts.find((addressPart) => addressPart.types.includes('locality'))?.long_name || '';
+				addressParts.find((addressPart) => addressPart.types.includes('locality'))?.long_name;
 			setAddress(searchedAddress);
 		}
 	};
