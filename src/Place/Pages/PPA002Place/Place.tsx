12c3dc45134f766c302298ddd04202fe72776b91
Takapy1 <takahirog1109mai@gmail.com>
2022-11-13 22:36:01 +0900

    Icon 配置

--- src/Place/Pages/PPA002Place/Place.tsx
+++ src/Place/Pages/PPA002Place/Place.tsx
@@ -1,7 +1,7 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React, { ReactNode, useContext, useEffect } from 'react';
 import { FlatList, View, Image, Linking, ScrollView } from 'react-native';
-import { Text, ActivityIndicator } from 'react-native-paper';
+import { Text, ActivityIndicator, Button } from 'react-native-paper';
 
 import { styles } from './PlaceStyle';
 
@@ -11,10 +11,22 @@ import { PMC01201GoogleMapPlaceOne } from '@/Place/Models/PDB01MPlace/Contexts/P
 import { PMC01203PlaceImage } from '@/Place/Models/PDB01MPlace/Contexts/PCT012MPlaceOne/ModelComponents/PMC01203PlaceImage';
 import { PCT012MPlaceOne } from '@/Place/Models/PDB01MPlace/Contexts/PCT012MPlaceOne/MPlaceOne';
 import { MPlaceOpeningHoursInterface } from 'spelieve-common/lib/Interfaces';
+import { PPA002PlaceController } from './PlaceController';
+import BookIcon from 'react-native-vector-icons/Entypo';
+import FetherIcon from 'react-native-vector-icons/Feather'
+import WebIcon from 'react-native-vector-icons/Foundation'
+import TimeIcon from 'react-native-vector-icons/Ionicons'
 
 export const PPA002Place = ({ route, navigation }: NativeStackScreenProps<BottomTabParamList, 'PPA002Place'>) => {
 	const { place, setPlaceId } = useContext(PCT012MPlaceOne);
 	const { place_id, language } = route.params;
+	const {
+		onCreateItineraryClicked, 
+		onShowMoreDetailClicked,
+		onShowMoreReviewClicked,
+		onImageClicked,
+		onShowMoreImageClicked,
+	} = PPA002PlaceController(route.params);
 
 	useEffect(() => {
 		setPlaceId(place_id);
@@ -58,13 +70,22 @@ export const PPA002Place = ({ route, navigation }: NativeStackScreenProps<Bottom
 					<PMC01201GoogleMapPlaceOne />
 					<PMC01203PlaceImage />
 					<Text style={styles.infoText}>{place.name}</Text>
-					<Text style={styles.infoText}>{place.formatted_address}</Text>
-					<Text style={styles.urlLink} onPress={() => Linking.openURL(`${place.website}`)}>
-						{place.website}
-					</Text>
-					<Text style={styles.infoText}>{place.formatted_phone_number}</Text>
+					<BookIcon name='open-book' size={50} onPress={() => onCreateItineraryClicked(place.name)} />
+					<FetherIcon name='map-pin' size={20}>
+						<Text style={styles.infoText}>{place.formatted_address}</Text>
+					</FetherIcon>
+					<WebIcon name='web' size={20}>
+						<Text style={styles.urlLink} onPress={() => Linking.openURL(`${place.website}`)}>
+							{place.website}
+						</Text>
+					</WebIcon>
+					<FetherIcon name='phone-call' size={20}>
+						<Text style={styles.infoText}>{place.formatted_phone_number}</Text>
+					</FetherIcon>
 					<View>
-						<Text style={styles.infoText}>{i18n.t('Opening Hours')}</Text>
+						<TimeIcon name='time-outline' size={20}>
+							<Text style={styles.infoText}>{i18n.t('Opening Hours')}</Text>
+						</TimeIcon>
 						{outputOpeningHour(place.openingHours)}
 					</View>
 				</View>
--- src/Place/Pages/PPA002Place/PlaceController.ts
+++ src/Place/Pages/PPA002Place/PlaceController.ts
@@ -1,3 +1,33 @@
-import { PlaceControllerInterface } from 'spelieve-common/lib/Interfaces';
+import { BottomTabParamList } from '@/App';
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack/lib/typescript/src/types';
+import { PlaceControllerInterface, PlacePropsInterface } from 'spelieve-common/lib/Interfaces';
 
-export const PPA002PlaceController = (): PlaceControllerInterface => {};
+export const PPA002PlaceController = ({place_id, language}: PlacePropsInterface): PlaceControllerInterface => {
+    const navigation = useNavigation<NativeStackNavigationProp<BottomTabParamList>>();
+
+    const onCreateItineraryClicked = (placeName: string) => {
+        navigation.navigate('Place', {
+            screen: 'IPA001ItineraryEdit',
+            params: { place_id, placeName }
+        });
+    };
+
+    const onShowMoreDetailClicked = () => {
+        
+    };
+
+    const onShowMoreReviewClicked = () => {
+        
+    };
+
+    const onImageClicked = () => {
+        
+    };
+
+    const onShowMoreImageClicked = () => {
+        
+    };
+
+    return {onCreateItineraryClicked, onShowMoreDetailClicked, onShowMoreReviewClicked, onImageClicked, onShowMoreImageClicked }
+};
