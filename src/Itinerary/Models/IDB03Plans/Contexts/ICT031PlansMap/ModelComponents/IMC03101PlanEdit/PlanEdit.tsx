76766d9797ea1d881b4049a14eff93a0f56d28e9
Ayato-kosaka <kosaka.ayato@gmail.com>
2022-11-14 20:46:22 +0900

    Update IMC03101PlanEdit

--- src/Itinerary/Models/IDB03Plans/Contexts/ICT031PlansMap/ModelComponents/IMC03101PlanEdit/PlanEdit.tsx
+++ src/Itinerary/Models/IDB03Plans/Contexts/ICT031PlansMap/ModelComponents/IMC03101PlanEdit/PlanEdit.tsx
@@ -11,6 +11,26 @@ import { ICT031PlansMap } from '../..';
 
 import i18n from '@/Common/Hooks/i18n-js';
 
+const subtraction = (base: Date, subtraction: Date, types: ('Month' | 'Date' | 'Hours' | 'Minutes' | 'Seconds')[]) => {
+	const ret = new Date(base.getTime());
+	if (types.includes('Month')) {
+		ret.setMonth(base.getMonth() - subtraction.getMonth());
+	}
+	if (types.includes('Date')) {
+		ret.setDate(base.getDate() - subtraction.getDate());
+	}
+	if (types.includes('Hours')) {
+		ret.setHours(base.getHours() - subtraction.getHours());
+	}
+	if (types.includes('Minutes')) {
+		ret.setMinutes(base.getMinutes() - subtraction.getMinutes());
+	}
+	if (types.includes('Seconds')) {
+		ret.setSeconds(base.getSeconds() - subtraction.getSeconds());
+	}
+	return ret;
+};
+
 export function IMC03101PlanEdit({
 	planID,
 	beforeAfterRepresentativeType,
@@ -29,7 +49,22 @@ export function IMC03101PlanEdit({
 	const planDocSnap = plansDocSnapMap[planID];
 	const plan = planDocSnap.data();
 
-	console.log('debug', 'PlanEdit', planID, plan);
+
+	// before の placeStartTime を設定する
+	useEffect(() => {
+		if (isPlanGroupMounted && beforeAfterRepresentativeType === 'before') {
+			console.log('debug', planID, 'before の placeStartTime を設定する');
+			// eslint-disable-next-line @typescript-eslint/no-floating-promises
+			setDoc(
+				planDocSnap.ref,
+				{
+					placeStartTime: subtraction(plan.placeEndTime, plan.placeSpan, ['Hours', 'Minutes']),
+				},
+				{ merge: true },
+			);
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [plan.placeSpan.getTime(), plan.placeEndTime.getTime(), beforeAfterRepresentativeType]);
 
 	// representative の placeStartTime を設定する
 	useEffect(() => {
@@ -45,7 +80,7 @@ export function IMC03101PlanEdit({
 			);
 		}
 		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [planGroupsDoc.data().representativeStartDateTime.getTime()]);
+	}, [planGroupsDoc.data().representativeStartDateTime.getTime(), beforeAfterRepresentativeType]);
 
 	// after の placeStartTime を設定する
 	useEffect(() => {
@@ -68,19 +103,37 @@ export function IMC03101PlanEdit({
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 		plansDocSnapMap[dependentPlanID].data().transportationArrivalTime?.getTime() ||
 			plansDocSnapMap[dependentPlanID].data().placeEndTime.getTime(),
+			beforeAfterRepresentativeType
+	]);
+
+	// before の placeEndTime を設定する
+	useEffect(() => {
+		if (isPlanGroupMounted && beforeAfterRepresentativeType === 'before') {
+			console.log('debug', planID, 'before の placeEndTime を設定する');
+			// eslint-disable-next-line @typescript-eslint/no-floating-promises
+			setDoc(
+				planDocSnap.ref,
+				{
+					placeEndTime:
+						plan.transportationDepartureTime ||
+						plansDocSnapMap[dependentPlanID].data().placeStartTime,
+				},
+				{ merge: true },
+			);
+			// console.log("finish setDoc", planDocSnap)
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+						plansDocSnapMap[planID].data().transportationDepartureTime?.getTime() ||
+						plansDocSnapMap[dependentPlanID].data().placeStartTime.getTime(),
+						beforeAfterRepresentativeType
 	]);
 
 	// representative, after の placeEndTime を設定する
 	useEffect(() => {
 		if (isPlanGroupMounted && ['representative', 'after'].includes(beforeAfterRepresentativeType)) {
-			console.log(
-				'debug',
-				planID,
-				'representative, after の placeEndTime を設定する',
-				plan.placeStartTime.getTime(),
-				plan.placeSpan.getTime(),
-				beforeAfterRepresentativeType,
-			);
+			console.log('debug', planID, 'representative, after の placeEndTime を設定する');
 			// eslint-disable-next-line @typescript-eslint/no-floating-promises
 			setDoc(
 				planDocSnap.ref,
