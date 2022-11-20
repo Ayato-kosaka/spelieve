cc1384de25bac8204c3c70f28b135bec82f30f9e
Ayato-kosaka <kosaka.ayato@gmail.com>
2022-11-20 16:02:19 +0000

    Update IPA003EditPlan

--- src/Itinerary/Pages/IPA003EditPlan/EditPlan.tsx
+++ src/Itinerary/Pages/IPA003EditPlan/EditPlan.tsx
@@ -1,10 +1,51 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { View } from 'react-native';
+import { useContext, useEffect, useMemo, useCallback } from 'react';
+import { ActivityIndicator, Image, View } from 'react-native';
 
 import { BottomTabParamList } from '@/App';
+import { ICT011ItineraryOne } from '@/Itinerary/Models/IDB01Itineraries/Contexts/ICT011ItineraryOne';
+import { ICT021PlanGroupsList } from '@/Itinerary/Models/IDB02PlanGroups/Contexts/ICT021PlanGroupsList';
+import { ICT031PlansMap } from '@/Itinerary/Models/IDB03Plans/Contexts/ICT031PlansMap';
 
 export const IPA003EditPlan = ({ route, navigation }: NativeStackScreenProps<BottomTabParamList, 'IPA003EditPlan'>) => {
 	const { itineraryID, planID } = route.params;
 
-	return <View>IPA003EditPlan</View>;
+	const { setItineraryID, itineraryDocSnap } = useContext(ICT011ItineraryOne);
+	const { isPlansLoading, plansDocSnapMap } = useContext(ICT031PlansMap);
+	const { planGroupsQSnap, planGroupsCRef } = useContext(ICT021PlanGroupsList);
+	const planDocSnap = useMemo(() => (planID ? plansDocSnapMap[planID] : undefined), [planID, plansDocSnapMap]);
+	const plan = useMemo(() => (planDocSnap ? planDocSnap.data() : undefined), [planDocSnap]);
+
+	// TODO: Conroller に移動する
+
+	useEffect(() => {
+		if (itineraryID) {
+			setItineraryID(itineraryID);
+		}
+	}, [itineraryID, setItineraryID]);
+
+	const navigate = useCallback(() => {
+		// TODO エラーを解消する
+		navigation.navigate('Itinerary', {
+			screen: 'IPA001ItineraryEdit',
+			params: {
+				itineraryID,
+			},
+		});
+	}, [navigation, itineraryID]);
+
+	if (!itineraryDocSnap || isPlansLoading || !planGroupsQSnap) {
+		return <ActivityIndicator animating />;
+	}
+
+	if (!itineraryDocSnap.exists() || !plan) {
+		navigate();
+		return <ActivityIndicator animating />;
+	}
+
+	return (
+		<View>
+			<Image source={{ uri: plan.imageUrl }} />
+		</View>
+	);
 };
