8f5813e70f0ca040af3559de56bcad8b5e9b3e7f
Ayato-kosaka <kosaka.ayato@gmail.com>
2022-12-03 02:04:21 +0000

    Update GooglePlaceLanguageTagFromIETFLanguageTag

--- src/Place/Hooks/PHK001GooglePlaceAPI.ts
+++ src/Place/Hooks/PHK001GooglePlaceAPI.ts
@@ -1,4 +1,6 @@
-export const GooglePlaceLanguageTagFromIETFLanguageTag: { [key: string]: string } = {
-	ja: 'ja',
-	en: 'en',
+import { Language } from "@googlemaps/google-maps-services-js";
+
+export const GooglePlaceLanguageTagFromIETFLanguageTag: { [key: string]: Language } = {
+	ja: Language.ja,
+	en: Language.en,
 };
