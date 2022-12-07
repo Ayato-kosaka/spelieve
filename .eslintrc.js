2ef55225b07618586e60a281f341640dbfb332e8
Ayato-kosaka <kosaka.ayato@gmail.com>
2022-12-07 16:06:28 +0000

    react/jsx-key

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,65 +1,58 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    tsconfigRootDir: './',
-    project: ['./tsconfig.json']
-  },
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-    'autofix',
-    'import',
-    'unused-imports'
-  ],
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    'airbnb-typescript',
-    'airbnb/hooks',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'prettier',
-  ],
-  "ignorePatterns": ["build"], 
-  rules: {
-    "import/extensions": "off",
-    'react/style-prop-object': 'off',
-    'react/react-in-jsx-scope': 'off',
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    "@typescript-eslint/no-unused-vars": "off",
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    'no-param-reassign': 'off',
-    "unused-imports/no-unused-imports": "warn",
-    "autofix/no-plusplus": "error",
-    "import/prefer-default-export": 'off',
-    "import/order": [
-      "warn",
-      {
-        "groups": ["builtin", "external", "internal", "parent", "sibling", "index", "object", "type"],
-        "newlines-between": "always",
-        "pathGroupsExcludedImportTypes": ["builtin"],
-        "alphabetize": { "order": "asc", "caseInsensitive": true },
-        "pathGroups": [
-          { "pattern": "@nestjs/**", "group": "internal", "position": "before" },
-          { "pattern": "spelieve-common/**", "group": "internal", "position": "before" },
-        ]
-      }
-    ],
-    "react/function-component-definition": [
-      2,
-      { "namedComponents": "arrow-function" }
-    ]
-  },
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+		tsconfigRootDir: './',
+		project: ['./tsconfig.json'],
+	},
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	plugins: ['react', '@typescript-eslint', 'autofix', 'import', 'unused-imports'],
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'airbnb',
+		'airbnb-typescript',
+		'airbnb/hooks',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+		'prettier',
+	],
+	ignorePatterns: ['build'],
+	rules: {
+		'import/extensions': 'off',
+		'react/style-prop-object': 'off',
+		'react/react-in-jsx-scope': 'off',
+		'react/jsx-key': 'error',
+		'@typescript-eslint/interface-name-prefix': 'off',
+		'@typescript-eslint/explicit-function-return-type': 'off',
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/no-unused-vars': 'off',
+		'@typescript-eslint/no-non-null-assertion': 'off',
+		'no-param-reassign': 'off',
+		'unused-imports/no-unused-imports': 'warn',
+		'autofix/no-plusplus': 'error',
+		'import/prefer-default-export': 'off',
+		'import/order': [
+			'warn',
+			{
+				groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object', 'type'],
+				'newlines-between': 'always',
+				pathGroupsExcludedImportTypes: ['builtin'],
+				alphabetize: { order: 'asc', caseInsensitive: true },
+				pathGroups: [
+					{ pattern: '@nestjs/**', group: 'internal', position: 'before' },
+					{ pattern: 'spelieve-common/**', group: 'internal', position: 'before' },
+				],
+			},
+		],
+		'react/function-component-definition': [2, { namedComponents: 'arrow-function' }],
+	},
 };
