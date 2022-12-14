1168ede0d3f55f29573127218f0903867976d2c4
Ayato-kosaka <kosaka.ayato@gmail.com>
2022-12-14 22:44:01 +0000

    close #346

--- src/Itinerary/Pages/IPA001ItineraryEdit/ItineraryEdit.tsx
+++ src/Itinerary/Pages/IPA001ItineraryEdit/ItineraryEdit.tsx
@@ -1,10 +1,11 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React, { useContext } from 'react';
-import { ScrollView, ActivityIndicator } from 'react-native';
+import { ScrollView, ActivityIndicator, Button } from 'react-native';
 
 import { IPA001ItineraryEditController } from './ItineraryEditController';
 
 import { BottomTabParamList } from '@/App';
+import i18n from '@/Common/Hooks/i18n-js';
 import { ICT011ItineraryOne } from '@/Itinerary/Models/IDB01Itineraries/Contexts/ICT011ItineraryOne';
 import { ICT021PlanGroupsList } from '@/Itinerary/Models/IDB02PlanGroups/Contexts/ICT021PlanGroupsList';
 import { ICT031PlansMap } from '@/Itinerary/Models/IDB03Plans/Contexts/ICT031PlansMap';
@@ -14,9 +15,9 @@ export const IPA001ItineraryEdit = ({
 	route,
 	navigation,
 }: NativeStackScreenProps<BottomTabParamList, 'IPA001ItineraryEdit'>) => {
-	const { itineraryDocSnap, itineraryCRef } = useContext(ICT011ItineraryOne);
+	const { itineraryDocSnap } = useContext(ICT011ItineraryOne);
 	const { isPlansLoading } = useContext(ICT031PlansMap);
-	const { planGroupsQSnap } = useContext(ICT021PlanGroupsList);
+	const { planGroupsQSnap, createPlanGroup } = useContext(ICT021PlanGroupsList);
 
 	const { createItinerary } = IPA001ItineraryEditController({
 		route,
@@ -38,6 +39,13 @@ export const IPA001ItineraryEdit = ({
 			{planGroupsQSnap?.docs.map((planGroupsDoc) => (
 				<IMC03103PlanGroupsEdit key={planGroupsDoc.id} planGroupsDoc={planGroupsDoc} />
 			))}
+			<Button
+				title={i18n.t('予定グループを追加')}
+				onPress={() => {
+					// eslint-disable-next-line @typescript-eslint/no-floating-promises
+					createPlanGroup();
+				}}
+			/>
 		</ScrollView>
 	);
 };
