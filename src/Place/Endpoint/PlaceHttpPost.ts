0aae104aae981a5a7b2f2649e22cbde6d97c98bb
Ayato-kosaka <kosaka.ayato@gmail.com>
2022-12-24 08:40:40 +0000

    add logger to PlaceHttpPost

--- src/Place/Endpoint/PlaceHttpPost.ts
+++ src/Place/Endpoint/PlaceHttpPost.ts
@@ -1,10 +1,12 @@
+import { Logger } from '@/Common/Hooks/CHK001Utils';
 import { ENV } from '@/ENV';
 
 export const PlaceHttpPost = async <RequestBodyType, ResponseType>(
 	target: string,
 	body: RequestBodyType,
-): Promise<ResponseType> =>
-	fetch(ENV.BACKEND_PLACE_ENDPOINT + target, {
+): Promise<ResponseType> => {
+	Logger('PlaceHttpPost', `RequestBody(${target})`, body);
+	return fetch(ENV.BACKEND_PLACE_ENDPOINT + target, {
 		method: 'POST',
 		headers: {
 			Accept: 'application/json',
@@ -22,3 +24,4 @@ export const PlaceHttpPost = async <RequestBodyType, ResponseType>(
 		}
 		return Promise.resolve(resJSON);
 	});
+};
