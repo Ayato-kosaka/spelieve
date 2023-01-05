7d392dac7e11615c9f4beb4ff3129e7202317e30
Ayato-kosaka <kosaka.ayato@gmail.com>
2023-01-05 13:57:08 +0000

    setShowPicker(false) を追加

--- src/Common/Components/CCO003DateTimePicker/DateTimePicker.tsx
+++ src/Common/Components/CCO003DateTimePicker/DateTimePicker.tsx
@@ -2,22 +2,34 @@ import RNDateTimePicker, {
 	IOSNativeProps,
 	AndroidNativeProps,
 	WindowsNativeProps,
+	DateTimePickerEvent,
 } from '@react-native-community/datetimepicker';
 import { useCallback, useState } from 'react';
+import { Platform } from 'react-native';
 import { Button } from 'react-native-paper';
 
 import i18n from '@/Common/Hooks/i18n-js';
 
 export const CCO003DateTimePicker = ({ ...props }: IOSNativeProps & AndroidNativeProps & WindowsNativeProps) => {
-	const [showPicker, setShowPicker] = useState<boolean>(false); // Platform.OS !== 'android'
+	const [showPicker, setShowPicker] = useState<boolean>(Platform.OS !== 'android');
 
 	const onButtonPress = useCallback(() => {
 		setShowPicker(true);
 	}, [setShowPicker]);
 
+	const onChange = useCallback(
+		(event: DateTimePickerEvent, date?: Date | undefined) => {
+			props.onChange?.(event, date);
+			if (Platform.OS === 'android') {
+				setShowPicker(false);
+			}
+		},
+		[props],
+	);
+
 	if (showPicker) {
 		// eslint-disable-next-line react/jsx-props-no-spreading
-		return <RNDateTimePicker {...props} />;
+		return <RNDateTimePicker {...props} onChange={onChange} />;
 	}
 	return (
 		<Button onPress={onButtonPress}>{props.mode === 'time' ? i18n.t('select time') : i18n.t('select date')}</Button>
