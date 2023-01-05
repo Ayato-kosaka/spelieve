8bffc884a6577725e11e5a1117bd2a49942aa110
Ayato-kosaka <kosaka.ayato@gmail.com>
2023-01-05 12:39:35 +0000

    button 追加

--- src/Common/Components/CCO003DateTimePicker/DateTimePicker.tsx
+++ src/Common/Components/CCO003DateTimePicker/DateTimePicker.tsx
@@ -3,8 +3,23 @@ import RNDateTimePicker, {
 	AndroidNativeProps,
 	WindowsNativeProps,
 } from '@react-native-community/datetimepicker';
+import { useCallback, useState } from 'react';
+import { Button } from 'react-native-paper';
 
-export const CCO003DateTimePicker = ({ ...props }: IOSNativeProps & AndroidNativeProps & WindowsNativeProps) => (
-	// eslint-disable-next-line react/jsx-props-no-spreading
-	<RNDateTimePicker {...props} />
-);
+import i18n from '@/Common/Hooks/i18n-js';
+
+export const CCO003DateTimePicker = ({ ...props }: IOSNativeProps & AndroidNativeProps & WindowsNativeProps) => {
+	const [showPicker, setShowPicker] = useState<boolean>(false); // Platform.OS !== 'android'
+
+	const onButtonPress = useCallback(() => {
+		setShowPicker(true);
+	}, [setShowPicker]);
+
+	if (showPicker) {
+		// eslint-disable-next-line react/jsx-props-no-spreading
+		return <RNDateTimePicker {...props} />;
+	}
+	return (
+		<Button onPress={onButtonPress}>{props.mode === 'time' ? i18n.t('select time') : i18n.t('select date')}</Button>
+	);
+};
