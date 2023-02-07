dabe7c08753e3897b544910e2658e72392b1ba3f
Takapy1 <takahirog1109mai@gmail.com>
2023-02-07 18:06:00 +0900

    add android unitID

--- src/Common/Components/CCO007GoogleBannerAd/GoogleBannerAd.tsx
+++ src/Common/Components/CCO007GoogleBannerAd/GoogleBannerAd.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { BannerAd, BannerAdSize, TestIds } from 'react-native-google-mobile-ads';
 import { ENV } from '@/ENV';
+import { Platform } from 'react-native';
 
-const adUnitId: string = __DEV__ ? TestIds.BANNER : ENV.ADMOB_IOS_Banner_UNIT_ID;
+const ProductionAdUnitID: string = Platform.select<string>({
+  ios: ENV.ADMOB_IOS_Banner_UNIT_ID,
+  android: ENV.ADMOB_ANDROID_Banner_UNIT_ID,
+})!;
+const adUnitID = __DEV__ ? TestIds.BANNER : ProductionAdUnitID;
 
 export const GoogleBannerAd = () => {
   return (
     <BannerAd
-      unitId={adUnitId}
+      unitId={adUnitID}
       size={BannerAdSize.FULL_BANNER}
       requestOptions={{
         requestNonPersonalizedAdsOnly: true,
