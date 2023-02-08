e0c7130706b4d551c7455cf18cdc4002ab1fa22b
Takapy1 <takahirog1109mai@gmail.com>
2023-02-09 08:12:14 +0900

    addnn sample View to web adsense comp

---
+++ src/Common/Components/CCO007GoogleBannerAd/GoogleBannerAd.web.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export const GoogleBannerAd = () => {
+    console.log("DEBUG: Web Banner Ad called")
+    return <View></View>;
+};
