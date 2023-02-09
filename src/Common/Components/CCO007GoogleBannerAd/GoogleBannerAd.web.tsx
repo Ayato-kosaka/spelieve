38ec372fe8af373631b11dc81c469c3d4a44dd74
Takapy1 <takahirog1109mai@gmail.com>
2023-02-09 22:49:15 +0900

    merge adsense

--- src/Common/Components/CCO007GoogleBannerAd/GoogleBannerAd.web.tsx
+++ src/Common/Components/CCO007GoogleBannerAd/GoogleBannerAd.web.tsx
@@ -1,7 +1,16 @@
+import { Adsense } from '@ctrl/react-adsense';
 import React from 'react';
 import { View } from 'react-native';
 
-export const GoogleBannerAd = () => {
-    console.log("DEBUG: Web Banner Ad called")
-    return <View></View>;
-};
+import i18n from '@/Common/Hooks/i18n-js';
+import { ENV } from '@/ENV';
+
+export const GoogleBannerAd = () =>
+	ENV.LOGGER ? (
+		<View>{i18n.t('Advertisement Area')}</View>
+	) : (
+		<Adsense
+			client="ca-pub-xxx" // TODO: 広告審査通過後に設定
+			slot="xxxxxxxxx" // TODO: 広告審査通過後に設定
+		/>
+	);
