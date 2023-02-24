80f2eafcae0c8aa92702dd2f9efc755d07877d1b
Ayato-kosaka <53715839+Ayato-kosaka@users.noreply.github.com>
2023-02-24 21:51:23 +0900

    Update GoogleBannerAd.web.tsx

--- src/Common/Components/CCO007GoogleBannerAd/GoogleBannerAd.web.tsx
+++ src/Common/Components/CCO007GoogleBannerAd/GoogleBannerAd.web.tsx
@@ -1,13 +1,14 @@
 import { Adsense } from '@ctrl/react-adsense';
 import React from 'react';
 import { View } from 'react-native';
+import { Text } from 'react-native-paper';
 
 import i18n from '@/Common/Hooks/i18n-js';
 import { ENV } from '@/ENV';
 
 export const CCO007GoogleBannerAd = () =>
 	ENV.LOGGER ? (
-		<View>{i18n.t('Advertisement Area')}</View>
+		<View><Text>{i18n.t('Advertisement Area')}</Text></View>
 	) : (
 		<Adsense
 			client="ca-pub-xxx" // TODO: 広告審査通過後に設定
