bf03510deb693687d634425a25519a285ca2c6a8
Takapy1 <takahirog1109mai@gmail.com>
2023-02-25 12:04:14 +0900

    button 削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,21 @@
 import 'expo-dev-client';
 
 import registerRootComponent from 'expo/build/launch/registerRootComponent';
+import { Button } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { AppProvider } from './AppProvider';
 import { Navigation } from './Common/Navigation/Navigation';
 import { paperTheme } from './ThemeProvider';
-import analytics from '@react-native-firebase/analytics';
-import { Button } from 'react-native';
+
+import { sendAnalyticsLogEvent } from '@/Common/Hooks/Analytics/Analytics';
 
 export const App = () => (
 	<SafeAreaProvider>
 		<PaperProvider theme={paperTheme}>
 			<AppProvider>
 				<Navigation />
-				<Button
-					title="Add To Basket"
-					onPress={async () =>
-					await analytics().logEvent('basket', {
-						id: 3745092,
-						item: 'mens grey t-shirt',
-						description: ['round neck', 'long sleeved'],
-						size: 'L',
-					})
-					}
-				/>
 			</AppProvider>
 		</PaperProvider>
 	</SafeAreaProvider>
