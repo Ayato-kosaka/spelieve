aaace11e441188849b78a69ad1f78ff4a4c94ac8
Ayato-kosaka <kosaka.ayato@gmail.com>
2023-03-07 13:27:42 +0000

    #69 Web マスク不具合を解消

--- src/Thumbnail/Pages/TPA001ThumbnailEditor/MaskDecoration/MaskDecoration.web.tsx
+++ src/Thumbnail/Pages/TPA001ThumbnailEditor/MaskDecoration/MaskDecoration.web.tsx
@@ -8,23 +8,36 @@ import { GestureProviderInterface } from '@/Thumbnail/Components/TCO001GesturePr
 
 export const TPA001MaskDecoration = ({ decoration, imageURI, onEndMaskGesture }: MaskDecorationPropsInterface) => {
 	const maskRef = useRef<HTMLImageElement>(null);
-	useEffect(() => {
-		if (decoration?.maskUri === undefined) return;
-		const image = new Image();
-		image.src = decoration.maskUri;
-		image.onload = () => {
-			if (maskRef.current && decoration.maskUri) {
-				// for more infomation about css_mask_image read https://www.webdesignleaves.com/pr/css/css_mask_image.html
-				maskRef.current.style.webkitMaskImage = `url(${decoration.maskUri})`;
-				maskRef.current.style.maskImage = `url(${decoration.maskUri})`;
-				maskRef.current.style.webkitMaskRepeat = 'no-repeat';
-				maskRef.current.style.maskRepeat = 'no-repeat';
-				maskRef.current.style.webkitMaskSize = 'auto 100%';
-				maskRef.current.style.maskSize = 'auto 100%';
-				maskRef.current.draggable = false;
-			}
-		};
+	const initMasRef = useCallback(() => {
+		if (!maskRef.current) return;
+		// for more infomation about css_mask_image read https://www.webdesignleaves.com/pr/css/css_mask_image.html
+		maskRef.current.style.webkitMaskImage = `url(${
+			decoration?.maskUri ??
+			// TODO: 後で変える
+			'https://cdn-icons-png.flaticon.com/512/2107/2107776.png'
+		})`;
+		maskRef.current.style.maskImage = `url(${
+			decoration?.maskUri ??
+			// TODO: 後で変える
+			'https://cdn-icons-png.flaticon.com/512/2107/2107776.png'
+		})`;
+		maskRef.current.style.webkitMaskRepeat = 'no-repeat';
+		maskRef.current.style.maskRepeat = 'no-repeat';
+		maskRef.current.style.webkitMaskSize = 'auto 100%';
+		maskRef.current.style.maskSize = 'auto 100%';
+		maskRef.current.draggable = false;
 	}, [decoration?.maskUri]);
+	useEffect(() => {
+		if (!decoration) return;
+		if (decoration.decorationType === 'Image') {
+			if (imageURI === undefined) return;
+			const image = new Image();
+			image.src = imageURI;
+			image.onload = () => {
+				initMasRef();
+			};
+		}
+	}, [decoration, decoration?.maskUri, imageURI, initMasRef]);
 
 	const onAnimating = useCallback((event: GestureProviderInterface) => {
 		'worklet';
@@ -42,19 +55,21 @@ export const TPA001MaskDecoration = ({ decoration, imageURI, onEndMaskGesture }:
 	}
 	return (
 		<TCO001GestureProvider onEndGesture={onEndMaskGesture} onAnimating={onAnimating}>
-			<View />
-			{decoration.decorationType === 'Image' && (
-				<img
-					src={imageURI}
-					alt="decoration"
-					style={{
-						flex: 1,
-						objectFit: 'cover',
-					}}
-					ref={maskRef}
-				/>
-			)}
-			{decoration.decorationType === 'Figure' && <View />}
+			<View>
+				<View />
+				{decoration.decorationType === 'Image' && (
+					<img
+						src={imageURI}
+						alt="decoration"
+						style={{
+							flex: 1,
+							objectFit: 'cover',
+						}}
+						ref={maskRef}
+					/>
+				)}
+				{decoration.decorationType === 'Figure' && <View />}
+			</View>
 		</TCO001GestureProvider>
 	);
 };
