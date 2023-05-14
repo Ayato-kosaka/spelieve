a0b6eecfeb55ce963b1d723d0472c3d809b3ef07
Ayato-kosaka <kosaka.ayato@gmail.com>
2023-05-14 12:22:51 +0000

    version をインクリメント

--- app.config.ts
+++ app.config.ts
@@ -7,7 +7,7 @@ export default ({ config }: ConfigContext) => {
 			owner: 'spelieve',
 			slug: 'Spelieve',
 			scheme: 'spelieve',
-			version: '2.1.0',
+			version: '2.2.0',
 			orientation: 'portrait',
 			icon: './assets/favicon.png',
 			userInterfaceStyle: 'light',
@@ -27,7 +27,7 @@ export default ({ config }: ConfigContext) => {
 				config: {
 					googleMapsApiKey: process.env.GCP_API_KEY_IOS,
 				},
-				buildNumber: '3',
+				buildNumber: '4',
 				googleServicesFile: './GoogleService-Info.plist',
 			},
 			android: {
@@ -41,7 +41,7 @@ export default ({ config }: ConfigContext) => {
 						apiKey: process.env.GCP_API_KEY_ANDROID,
 					},
 				},
-				versionCode: 6, // TODO: 毎submitで変更の必要あり
+				versionCode: 7, // TODO: 毎submitで変更の必要あり
 				permissions: [],
 				googleServicesFile: './google-services.json',
 			},
