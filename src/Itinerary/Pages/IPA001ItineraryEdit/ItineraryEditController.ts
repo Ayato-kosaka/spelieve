8d6c1999fca9af9899bac0639a6f861b9a90c705
Takapy1 <takahirog1109mai@gmail.com>
2023-07-13 07:35:19 +0900

    fix format

--- src/Itinerary/Pages/IPA001ItineraryEdit/ItineraryEditController.ts
+++ src/Itinerary/Pages/IPA001ItineraryEdit/ItineraryEditController.ts
@@ -168,10 +168,7 @@ export const IPA001ItineraryEditController = ({ route, navigation }: ItineraryTo
 	const buildCopyItineraryPreviewDL = async () => {
 		if (!itineraryID) return;
 		const previewPageLink = `${ENV.HOST_NAME_WEB}ItineraryPreview?itineraryID=${itineraryID}`;
-		const url =
-			Platform.OS !== 'web'
-				? await buildDynamicLink(previewPageLink)
-				: previewPageLink;
+		const url = Platform.OS !== 'web' ? await buildDynamicLink(previewPageLink) : previewPageLink;
 		await setStringAsync(url);
 	};
 
