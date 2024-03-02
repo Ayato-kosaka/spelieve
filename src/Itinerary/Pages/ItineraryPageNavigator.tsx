616af453069bf060103c6cce86f7c0f8536b1626
Ayato-kosaka <kosaka.ayato@gmail.com>
2024-03-02 05:53:10 +0000

    #960 ボトムタブ押下時の挙動設定 ``` ■ 課題③ Web遷移の考慮漏れ 問題点: navigationEvent#tabPress では、最初の Stack に積まれている画面に遷移するが、 web の場合 URL から奥画面に直接遷移することがあり、初期画面に遷移するわけではない。
    初期画面に遷移する手段がない課題に対する対策: タブボタンから初期画面遷移を可能にする。
    想定外の挙動が発生するリスク: 課題①のケースは検討済み。それ以外は起きてみないとわからない。
    ```

--- src/Itinerary/Pages/ItineraryPageNavigator.tsx
+++ src/Itinerary/Pages/ItineraryPageNavigator.tsx
@@ -1,5 +1,5 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { useCallback } from 'react';
+import { useCallback, useEffect } from 'react';
 import { Pressable, Text } from 'react-native';
 
 import { INV002ItineraryTopTabNavigator } from '../Navigator/INV002ItineraryTopTabNavigator';
@@ -9,13 +9,34 @@ import { CPA001HelloSpelieve } from '@/Common/Pages/CPA001HelloSpelieve/HelloSpe
 import { materialColors } from '@/ThemeProvider';
 
 const Stack = createNativeStackNavigator<ItineraryStackParamList>();
+const routeName = 'Itinerary' as const;
+const initialRouteName = 'HelloSpelieve' as const;
 
-export const ItineraryPageNavigator = ({ navigation, route }: BottomTabNavigatorScreenProps<'Itinerary'>) => {
+export const ItineraryPageNavigator = ({ navigation, route }: BottomTabNavigatorScreenProps<typeof routeName>) => {
+	useEffect(() => {
+		// 外部関数に切り出すと想定外のエラーが起きるため個別ページに実装する。
+		const unsubscribe = navigation.addListener('tabPress', (e) => {
+			if (navigation.getState().routes[navigation.getState().index].name === routeName) {
+				// tabPress では、最初の Stack に積まれている画面に遷移するが、 web の場合 URL から奥画面に直接遷移することがあり、
+				// 最初の Stack が initialRouteScreen と同一とは限らないため、明示的に initialRouteScreen に遷移する実装を組み込む。
+				navigation.navigate('BottomTabNavigator', {
+					screen: routeName,
+					params: {
+						screen: initialRouteName,
+						params: {},
+					},
+				});
+				e.preventDefault();
+			}
+		});
+		return unsubscribe;
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 	const headerTitle = useCallback(
 		() => (
 			<Pressable
 				onPress={() =>
-					navigation.navigate('Itinerary', {
+					navigation.navigate(routeName, {
 						screen: 'HelloSpelieve',
 						params: {},
 					})
@@ -35,7 +56,7 @@ export const ItineraryPageNavigator = ({ navigation, route }: BottomTabNavigator
 	);
 	return (
 		<Stack.Navigator
-			initialRouteName="HelloSpelieve"
+			initialRouteName={initialRouteName}
 			screenOptions={{
 				headerStyle: {
 					backgroundColor: materialColors.orange['500'],
--- src/Place/Pages/PlacePageNavigator/PlacePageNavigator.tsx
+++ src/Place/Pages/PlacePageNavigator/PlacePageNavigator.tsx
@@ -1,21 +1,45 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useEffect } from 'react';
 
 import { PCT012MPlaceOneProvider } from '../../Contexts/PCT012MPlaceOne/MPlaceOne';
 import { PPA001Places } from '../PPA001Places';
 import { PPA002Place } from '../PPA002Place/Place';
 
-import { PlaceStackParamList } from '@/Common/Navigation/NavigationInterface';
+import { BottomTabNavigatorScreenProps, PlaceStackParamList } from '@/Common/Navigation/NavigationInterface';
 import { PCT011MPlacesListProvider } from '@/Place/Contexts/PCT011MPlacesList';
 
 const Stack = createNativeStackNavigator<PlaceStackParamList>();
+const routeName = 'Place' as const;
+const initialRouteName = 'PPA001Places' as const;
 
-export const PlacePageNavigator = () => (
-	<PCT012MPlaceOneProvider>
-		<PCT011MPlacesListProvider>
-			<Stack.Navigator initialRouteName="PPA001Places" screenOptions={{ headerShown: false }}>
-				<Stack.Screen name="PPA001Places" component={PPA001Places} initialParams={{}} />
-				<Stack.Screen name="PPA002Place" component={PPA002Place} initialParams={{}} />
-			</Stack.Navigator>
-		</PCT011MPlacesListProvider>
-	</PCT012MPlaceOneProvider>
-);
+export const PlacePageNavigator = ({ navigation, route }: BottomTabNavigatorScreenProps<typeof routeName>) => {
+	useEffect(() => {
+		// 外部関数に切り出すと想定外のエラーが起きるため個別ページに実装する。
+		const unsubscribe = navigation.addListener('tabPress', (e) => {
+			if (navigation.getState().routes[navigation.getState().index].name === routeName) {
+				// tabPress では、最初の Stack に積まれている画面に遷移するが、 web の場合 URL から奥画面に直接遷移することがあり、
+				// 最初の Stack が initialRouteScreen と同一とは限らないため、明示的に initialRouteScreen に遷移する実装を組み込む。
+				navigation.navigate('BottomTabNavigator', {
+					screen: routeName,
+					params: {
+						screen: initialRouteName,
+						params: {},
+					},
+				});
+				e.preventDefault();
+			}
+		});
+		return unsubscribe;
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+	return (
+		<PCT012MPlaceOneProvider>
+			<PCT011MPlacesListProvider>
+				<Stack.Navigator initialRouteName={initialRouteName} screenOptions={{ headerShown: false }}>
+					<Stack.Screen name="PPA001Places" component={PPA001Places} initialParams={{}} />
+					<Stack.Screen name="PPA002Place" component={PPA002Place} initialParams={{}} />
+				</Stack.Navigator>
+			</PCT011MPlacesListProvider>
+		</PCT012MPlaceOneProvider>
+	);
+};
